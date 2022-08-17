72277af137452e9dd4cf9c806aff3f7e7ef58662
psychehose <psychehose@gmail.com>
2022-08-17 19:13:23 +0900

    Initial Setting using Xcconfig - dev, staging, production

---
+++ Hous-iOS-release/Scene/Splash/SplashViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SplashViewController.swift
+//  Hous-iOS-release
+//
+//  Created by 김호세 on 2022/08/13.
+//
+
+import UIKit
+
+final class SplashViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    view.backgroundColor = .red
+    print("Splash")
+  }
+}
