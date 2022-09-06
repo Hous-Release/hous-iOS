0f5e433905bac49537a61f83ff974652664b6608
Jihyun247 <wlguslove12345@gmail.com>
2022-09-07 01:31:15 +0900

    .

---
+++ Hous-iOS-release/Scene/Profile/ProfileViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ProfileViewController.swift
+//  Hous-iOS-release
+//
+//  Created by 김지현 on 2022/09/02.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.view.backgroundColor = .blue
+  }
+}
---
+++ Hous-iOS-release/Scene/Todo/TodoViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TodoViewController.swift
+//  Hous-iOS-release
+//
+//  Created by 김지현 on 2022/09/02.
+//
+
+import UIKit
+
+class TodoViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.view.backgroundColor = .yellow
+  }
+}
