5e3ec51df1fc3668accb1024b4372cd38ce3f982
Jihyun247 <wlguslove12345@gmail.com>
2022-09-07 17:53:06 +0900

    [#4] FEAT : tabBar item

---
+++ Hous-iOS-release/Scene/TabBar/HousTabBarItem.swift
@@ -0,0 +1,60 @@
+//
+//  HousTabBarItem.swift
+//  Hous-iOS-release
+//
+//  Created by 김지현 on 2022/09/07.
+//
+
+import UIKit
+
+enum HousTabBarItem: String, CaseIterable {
+  case hous
+  case todo
+  case profile
+}
+
+extension HousTabBarItem {
+  var viewController: UIViewController {
+    switch self {
+    case .hous:
+      return UINavigationController(rootViewController: HousViewController())
+    case .todo:
+      return UINavigationController(rootViewController: TodoViewController())
+    case .profile:
+      return UINavigationController(rootViewController: ProfileViewController())
+    }
+  }
+
+  var unselectedIcon: UIImage {
+    switch self {
+    case .hous:
+      return Images.icHousNocheck.image
+    case .todo:
+      return Images.icTodoNocheck.image
+    case .profile:
+      return Images.icProfileNocheck.image
+    }
+  }
+
+  var selectedIcon: UIImage {
+    switch self {
+    case .hous:
+      return Images.icHousCheck.image
+    case .todo:
+      return Images.icTodoCheck.image
+    case .profile:
+      return Images.icProfileCheck.image
+    }
+  }
+
+  var name: String {
+    switch self {
+    case .hous:
+      return "Hous-"
+    case .todo:
+      return "To-Do"
+    case .profile:
+      return "Profile"
+    }
+  }
+}
---
+++ Hous-iOS-release/Scene/TabBar/HousTabBarItemView.swift
@@ -0,0 +1,86 @@
+//
+//  HousTabBarItemView.swift
+//  Hous-iOS-release
+//
+//  Created by 김지현 on 2022/09/07.
+//
+
+import UIKit
+import SnapKit
+
+final class HousTabBarItemView: UIView {
+
+  private let titleLabel: UILabel = {
+    let label = UILabel()
+    label.font = Fonts.Montserrat.semiBold.font(size: 13)
+    label.textAlignment = .center
+    return label
+  }()
+
+  private let iconImageView: UIImageView = {
+    let imageView = UIImageView()
+    return imageView
+  }()
+  private let containerView = UIView()
+
+  private let item: HousTabBarItem
+  let index: Int
+
+  var isSelected = false {
+    didSet {
+      animateItems()
+    }
+  }
+
+  init(item: HousTabBarItem, index: Int) {
+    self.item = item
+    self.index = index
+    super.init(frame: .zero)
+
+    titleLabel.text = item.name
+    iconImageView.image = isSelected ? item.selectedIcon : item.unselectedIcon
+
+    setupViews()
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  private func setupViews() {
+    addSubview(containerView)
+    containerView.addSubview(titleLabel)
+    containerView.addSubview(iconImageView)
+
+    containerView.snp.makeConstraints { make in
+      make.top.bottom.leading.trailing.equalToSuperview()
+    }
+
+    iconImageView.snp.makeConstraints { make in
+      make.size.equalTo(20)
+      make.top.centerX.equalToSuperview()
+    }
+
+    titleLabel.snp.makeConstraints { make in
+      make.top.equalTo(iconImageView.snp.bottom).offset(2)
+      make.leading.trailing.equalToSuperview()
+    }
+  }
+
+  private func animateItems() {
+    UIView.animate(withDuration: 0.4) { [weak self] in
+      guard let self = self else { return }
+      self.titleLabel.textColor = self.isSelected ? Colors.blue.color : Colors.blueL1.color
+    }
+  }
+
+  func animateClick(completion: @escaping () -> Void) {
+    UIView.animate(withDuration: 0.15) {
+      self.transform = CGAffineTransform(scaleX: 0.9, y: 0.9)
+    } completion: { _ in
+      UIView.animate(withDuration: 0.15) {
+        self.transform = CGAffineTransform.identity
+      } completion: { _ in completion() }
+    }
+  }
+}
