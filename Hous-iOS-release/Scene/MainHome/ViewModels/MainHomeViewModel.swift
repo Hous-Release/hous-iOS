674149a0d7e6b2f3728e6647a6e0ba410f539b0b
Kim Minjae <ffalswo2@naver.com>
2022-09-18 04:51:14 +0900

    [#7] DEL : 불필요한 파일 삭제

--- Hous-iOS-release/Scene/MainHome/ViewModels/MainHomeViewModel.swift
+++ Hous-iOS-release/Scene/MainHome/ViewModels/MainHomeViewModel.swift
@@ -12,7 +12,6 @@ import RxCocoa
 
 final class MainHomeViewModel: ViewModelType {
   
-  private let homeUseCase: MainHomeUseCase
   private let disposeBag = DisposeBag()
   
   //MARK: - Input
@@ -35,59 +34,12 @@ final class MainHomeViewModel: ViewModelType {
 //    let lottie: BehaviorRelay<String>
   }
   
-  init(homeUseCase: MainHomeUseCase) {
-    self.homeUseCase = homeUseCase
-  }
-  
   func transform(input: Input) -> Output {
     let output = Output()
-    self.bindOutput(output: output)
-    
-    input.viewWillAppear.subscribe { [weak self] _ in
-      
-      self?.homeUseCase.getHomeData()
-    }
-    .disposed(by: disposeBag)
     
     return output
   }
   
-  private func bindOutput(output: Output) {
-    let nickname = homeUseCase.userNickname
-    let roomName = homeUseCase.roomName
-    
-    let titleText = Observable.combineLatest(nickname, roomName) { nickname, roomName in
-      return "\(nickname)님의,\n \(roomName) 하우스"
-    }
-    
-    titleText.subscribe(onNext: { text in
-      output.titleText.accept(text)
-    })
-    .disposed(by: disposeBag)
-    
-    let progress = homeUseCase.progress
-    progress.subscribe(onNext: { progress in
-      output.todoProgress.accept(progress)
-    })
-    .disposed(by: disposeBag)
-    
-    let todoTexts = homeUseCase.todos
-    todoTexts.subscribe { todos in
-      output.todoTexts.accept(todos)
-    }
-    .disposed(by: disposeBag)
-    
-    let ourRulesTexts = homeUseCase.rules
-    ourRulesTexts.subscribe { rules in
-      output.ourRulesTexts.accept(rules)
-    }
-    .disposed(by: disposeBag)
-    
-    let homies = homeUseCase.homieProfiles
-    homies.subscribe { homeModels in
-      output.homies.accept(homeModels)
-    }
-    .disposed(by: disposeBag)
-  }
+  
   
 }
