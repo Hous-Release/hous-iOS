135fffdecb4d1c180c75c9099b97fe7ba35d1d7d
Kim Minjae <ffalswo2@naver.com>
2022-09-18 04:51:47 +0900

    [#7] FEAT : 방이름 변경 UI기능 구현

---
+++ Hous-iOS-release/Scene/MainHome/EditHousNameDTO.swift
@@ -0,0 +1,14 @@
+//
+//  EditHousNameDTO.swift
+//  Hous-iOS-release
+//
+//  Created by 김민재 on 2022/09/17.
+//
+
+import Foundation
+
+struct EditHousDTO: Codable {
+  let message: String
+  let status: Int
+  let success: Bool
+}
