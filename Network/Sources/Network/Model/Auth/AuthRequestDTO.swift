fd67afffb698332c43e37e408a4a5ef54ce75134
psychehose <psychehose@gmail.com>
2022-09-18 23:19:12 +0900

    [#13] ADD: Auth DTO 정의

--- Network/Sources/Network/Model/Auth/AuthRequestDTO.swift
+++ Network/Sources/Network/Model/Auth/AuthRequestDTO.swift
@@ -5,4 +5,23 @@
 //  Created by 김호세 on 2022/09/17.
 //
 
-import Foundation
+public extension AuthDTO.Request {
+  struct LoginRequestDTO: Encodable {
+
+    public let fcmToken: String
+    public let socialType: String
+    // MARK: - Access Token For Auth login
+    public let token: String
+
+    public init(
+      fcmToken: String,
+      socialType: String,
+      token: String
+    ) {
+      self.fcmToken = fcmToken
+      self.socialType = socialType
+      self.token = token
+    }
+
+  }
+}
