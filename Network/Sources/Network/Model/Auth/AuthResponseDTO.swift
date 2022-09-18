e8a6c098585e8497dd8b91f3a47900bdfbb468c4
psychehose <psychehose@gmail.com>
2022-09-18 23:19:12 +0900

    [#13] ADD: Auth DTO 정의

--- Network/Sources/Network/Model/Auth/AuthResponseDTO.swift
+++ Network/Sources/Network/Model/Auth/AuthResponseDTO.swift
@@ -5,4 +5,15 @@
 //  Created by 김호세 on 2022/09/17.
 //
 
-import Foundation
+public extension AuthDTO.Response {
+  struct LoginResponseDTO: Decodable {
+
+    public let token: Token
+    public let userID: Int
+
+    enum CodingKeys: String, CodingKey {
+      case token
+      case userID = "userId"
+    }
+  }
+}
