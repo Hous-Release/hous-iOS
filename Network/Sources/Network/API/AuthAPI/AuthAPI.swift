06c0affa68006f12c6750216431ecc3823437543
psychehose <psychehose@gmail.com>
2022-09-18 23:20:09 +0900

    [#13] ADD: Auth Login API 작성

--- Network/Sources/Network/API/AuthAPI/AuthAPI.swift
+++ Network/Sources/Network/API/AuthAPI/AuthAPI.swift
@@ -1,8 +1,30 @@
 //
-//  Dummy.swift
+//  AuthAPI.swift
 //  
 //
 //  Created by 김호세 on 2022/09/17.
 //
 
 import Foundation
+
+protocol AuthAPIProtocol {
+
+  func login(
+    _ loginRequestDTO: AuthDTO.Request.LoginRequestDTO,
+    completion: @escaping (BaseResponseType<AuthDTO.Response.LoginResponseDTO>?, Error?) -> Void
+  )
+}
+
+public final class AuthAPI: APIRequestLoader<AuthService>, AuthAPIProtocol {
+  public func login(
+    _ loginRequestDTO: AuthDTO.Request.LoginRequestDTO,
+    completion: @escaping (BaseResponseType<AuthDTO.Response.LoginResponseDTO>?, Error?) -> Void) {
+    fetchData(
+      target: .login(loginRequestDTO),
+      responseData: BaseResponseType<AuthDTO.Response.LoginResponseDTO>.self,
+      isWithInterceptor: false
+    ) { res, err in
+        completion(res, err)
+      }
+  }
+}
