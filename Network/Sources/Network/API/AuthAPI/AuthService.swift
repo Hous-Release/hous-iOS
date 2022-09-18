b6d6dad19b031220a64679a3abb7b9e721acce96
psychehose <psychehose@gmail.com>
2022-09-18 23:19:55 +0900

    [#13] ADD: Auth Service 작성

--- Network/Sources/Network/API/AuthAPI/AuthService.swift
+++ Network/Sources/Network/API/AuthAPI/AuthService.swift
@@ -1,8 +1,40 @@
 //
-//  File.swift
+//  AuthService.swift
 //  
 //
 //  Created by 김호세 on 2022/09/17.
 //
 
 import Foundation
+import Alamofire
+
+public enum AuthService {
+  case login(_ dto: AuthDTO.Request.LoginRequestDTO)
+}
+
+extension AuthService: TargetType {
+  public var baseURL: String {
+    return API.apiBaseURL
+  }
+
+  public var path: String {
+    switch self {
+    case .login:
+      return "auth/login"
+    }
+  }
+
+  public var method: HTTPMethod {
+    switch self {
+    case .login:
+      return .post
+    }
+  }
+
+  public var parameters: RequestParams {
+    switch self {
+    case let .login(dto):
+      return .body(dto)
+    }
+  }
+}
