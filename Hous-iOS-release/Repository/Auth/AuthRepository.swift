ffdf310a35c4a9cf1d3df1ba1cee78b067d3af27
psychehose <psychehose@gmail.com>
2022-09-23 21:13:06 +0900

    [#13] Chore: Auth Repository file, folder 추가

---
+++ Hous-iOS-release/Repository/Auth/AuthRepository.swift
@@ -0,0 +1,18 @@
+//
+//  AuthRepository.swift
+//  Hous-iOS-release
+//
+//  Created by 김호세 on 2022/09/23.
+//
+
+import Foundation
+import Network
+
+
+public protocol AuthRepository {
+//  func login()
+}
+public final class AuthRepositoryImp: AuthRepository {
+
+}
+
