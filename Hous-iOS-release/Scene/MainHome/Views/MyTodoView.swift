d35c2e5fed495d3b4f13f33a8f8e7b8edb315939
Kim Minjae <ffalswo2@naver.com>
2022-09-24 22:04:53 +0900

    [#7] ADD : MainHome Section, TodoView, RuleView 추가

---
+++ Hous-iOS-release/Scene/MainHome/Views/MyTodoView.swift
@@ -0,0 +1,20 @@
+//
+//  MyTodoView.swift
+//  Hous-iOS-release
+//
+//  Created by 김민재 on 2022/09/24.
+//
+
+import UIKit
+
+class MyTodoView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
