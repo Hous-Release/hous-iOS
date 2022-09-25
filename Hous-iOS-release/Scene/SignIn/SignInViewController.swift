82b563d5b1db3c6e115fa481b7c2346413c1ce23
psychehose <psychehose@gmail.com>
2022-09-25 18:03:21 +0900

    [#19] ADD: Kakao Login Test

--- Hous-iOS-release/Scene/SignIn/SignInViewController.swift
+++ Hous-iOS-release/Scene/SignIn/SignInViewController.swift
@@ -7,7 +7,9 @@
 
 import Foundation
 import UIKit
-import RxRelay
+
+import RxCocoa
+import RxSwift
 
 final class SignInViewController: UIViewController {
 
@@ -18,12 +20,15 @@ final class SignInViewController: UIViewController {
 
   private var signInRelay = PublishRelay<(String?, Error?)>()
 
-  private var appleLoginManager = AppleOAuthManager()
+  private let appleLoginManager = AppleOAuthManager()
+  private let kakaoLoginManager = KakaoOAuthManager()
 
   private let kakaoLoginButton: UIButton = {
     let button = UIButton()
     button.setTitle("카카오톡으로 계속하기", for: .normal)
     button.setTitleColor(Colors.yellow.color, for: .normal)
+    button.titleLabel?.font = Fonts.SpoqaHanSansNeo.bold.font(size: 16)
+    button.titleLabel?.textAlignment = .center
 
     return button
   }()
@@ -31,15 +36,24 @@ final class SignInViewController: UIViewController {
   private let appleLoginButton: UIButton = {
     let button = UIButton()
     button.setTitle("애플로그인", for: .normal)
-    button.setTitleColor(UIColor.white, for: .normal)
+    button.setTitleColor(UIColor.black, for: .normal)
+    button.titleLabel?.font = Fonts.SpoqaHanSansNeo.bold.font(size: 16)
+    button.titleLabel?.textAlignment = .center
     return button
   }()
+
+  private let disposeBag = DisposeBag()
 
 
 
   override func viewDidLoad() {
     super.viewDidLoad()
     configureAppleSignIn()
+    configureKakaoSignIn()
+
+    setupViews()
+
+    bind()
 
   }
 
@@ -52,14 +66,26 @@ final class SignInViewController: UIViewController {
       make.height.equalTo(50)
       make.centerY.equalToSuperview()
     }
-    appleLoginButton.snp.makeConstraints { make in
+
+    kakaoLoginButton.snp.makeConstraints { make in
       make.leading.trailing.equalToSuperview().inset(20)
       make.height.equalTo(50)
-      make.centerY.equalToSuperview()
+      make.top.equalTo(appleLoginButton.snp.bottom).offset(20)
     }
   }
 
   private func bind() {
+    kakaoLoginButton.rx.tap.map { _ in }
+      .map { self.kakaoLoginManager.login() }
+      .subscribe(onNext: {
+      })
+      .disposed(by: disposeBag)
+
+    signInRelay
+      .subscribe(onNext: {
+        print($0)
+    })
+      .disposed(by: disposeBag)
 
   }
 
@@ -79,4 +105,19 @@ extension SignInViewController {
       self?.signInRelay.accept((nil, error))
     }
   }
+
+  private func configureKakaoSignIn() {
+
+
+    kakaoLoginManager.configure(appKey: "23a6d7ad94f44f0e474ee41b4e6d9fab")
+
+    kakaoLoginManager.onSuccess = { [weak self] identifyToken, _ in
+      self?.signInRelay.accept((identifyToken, nil))
+    }
+
+    kakaoLoginManager.onFailure = { [weak self] error in
+      self?.signInRelay.accept((nil, error))
+    }
+  }
+
 }
