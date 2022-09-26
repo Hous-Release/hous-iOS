f0e068737234a08cd9a08119d0bc06fb141d964a
Jihyun247 <wlguslove12345@gmail.com>
2022-09-26 19:07:52 +0900

    [#15] FEAT : 방 입장 뷰 레이아웃

--- Hous-iOS-release/Scene/Onboarding/EnterRoom/EnterRoomView.swift
+++ Hous-iOS-release/Scene/Onboarding/EnterRoom/EnterRoomView.swift
@@ -12,14 +12,21 @@ import Then
 
 class EnterRoomView: UIView {
 
-  // 3. 공통버튼 만든거 불러와서 뷰 짜기
-  // 4. 탭제스쳐 rx랑 연결해서 뷰 전환 구현
-
   let guideTitleLabel = UILabel().then {
     $0.text = "이제 하우스를 입장해볼까요?"
     $0.font = Fonts.SpoqaHanSansNeo.bold.font(size: 20)
     $0.textColor = Colors.black.color
   }
+
+  let stackView = UIStackView().then {
+    $0.axis = .vertical
+    $0.spacing = 28
+    $0.distribution = .fillEqually
+    $0.alignment = .fill
+  }
+
+  let newRoomView = EnterRoomButtonView(roomType: .new)
+  let existRoomView = EnterRoomButtonView(roomType: .exist)
 
   override init(frame: CGRect) {
     super.init(frame: frame)
@@ -31,6 +38,17 @@ class EnterRoomView: UIView {
   }
 
   private func render() {
+    addSubViews([guideTitleLabel, stackView])
+    stackView.addArrangedSubviews(newRoomView, existRoomView)
 
+    guideTitleLabel.snp.makeConstraints { make in
+      make.centerX.equalToSuperview()
+    }
+
+    stackView.snp.makeConstraints { make in
+      make.top.equalTo(guideTitleLabel.snp.bottom).offset(64)
+      make.leading.trailing.equalToSuperview().inset(24)
+      make.centerY.equalToSuperview().multipliedBy(1.1)
+    }
   }
 }
