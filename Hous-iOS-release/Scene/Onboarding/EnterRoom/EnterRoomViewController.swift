510fe22a8401d5eb283757b75f4a402732b2f66f
Jihyun247 <wlguslove12345@gmail.com>
2022-09-26 18:25:57 +0900

    [#15] FEAT : 방 입장 뷰 버튼

--- Hous-iOS-release/Scene/Onboarding/EnterRoom/EnterRoomViewController.swift
+++ Hous-iOS-release/Scene/Onboarding/EnterRoom/EnterRoomViewController.swift
@@ -5,21 +5,21 @@
 //  Created by 김지현 on 2022/09/25.
 //
 
-//import UIKit
-//import RxCocoa
-//import ReactorKit
-//
-//class EnterRoomViewController: UIViewController, View {
-//
-//  var disposeBag = DisposeBag()
-//
-//  override func loadView() {
-//    super.loadView()
-//
-//  }
-//
-//  override func viewDidLoad() {
-//    super.viewDidLoad()
-//
-//  }
-//}
+import UIKit
+import RxCocoa
+import ReactorKit
+
+class EnterRoomViewController: UIViewController, View {
+
+  var disposeBag = DisposeBag()
+
+  override func loadView() {
+    super.loadView()
+
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+  }
+}
