678909b70f5d3fcc2d30ac5b9fe5b3f6bc127f7c
Jihyun247 <wlguslove12345@gmail.com>
2022-09-28 13:04:37 +0900

    [#15] FEAT : 방 입장 뷰 기능 구현

--- Hous-iOS-release/Scene/Onboarding/EnterRoom/EnterRoomViewController.swift
+++ Hous-iOS-release/Scene/Onboarding/EnterRoom/EnterRoomViewController.swift
@@ -7,11 +7,10 @@
 
 import UIKit
 import RxCocoa
+import RxGesture
 import ReactorKit
 
-class EnterRoomViewController: UIViewController {
-
-  // 영역 터치 시 뷰 넘김
+class EnterRoomViewController: UIViewController, View {
 
   var mainView = EnterRoomView()
   var disposeBag = DisposeBag()
@@ -23,6 +22,61 @@ class EnterRoomViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
+    reactor = EnterRoomViewReactor()
+  }
 
+  func bind(reactor: EnterRoomViewReactor) {
+    bindAction(reactor)
+    bindStatus(reactor)
+  }
+}
+
+extension EnterRoomViewController {
+  private func bindAction(_ reactor: EnterRoomViewReactor) {
+    mainView.newRoomView.rx.tapGesture()
+      .when(.recognized)
+      .withUnretained(self)
+      .map { _ in Reactor.Action.didTapNewRoomButton }
+      .bind(to: reactor.action)
+      .disposed(by: disposeBag)
+
+    mainView.existRoomView.rx.tapGesture()
+      .when(.recognized)
+      .withUnretained(self)
+      .map { _ in Reactor.Action.didTapExistRoomButton }
+      .bind(to: reactor.action)
+      .disposed(by: disposeBag)
+  }
+
+  private func bindStatus(_ reactor: EnterRoomViewReactor) {
+    reactor.state.map { $0.newRoomTransition }
+      .subscribe (onNext: { [weak self] isTapped in
+        if isTapped {
+          let vc = CreateNewRoomViewController()
+          self?.mainView.newRoomView.animateClick {
+            self?.presentViewController(vc)
+          }
+        }
+      })
+      .disposed(by: disposeBag)
+
+    reactor.state.map { $0.existRoomTransition }
+      .subscribe (onNext: { [weak self] isTapped in
+        if isTapped {
+          let vc = EnterRoomCodeViewController()
+          self?.mainView.existRoomView.animateClick {
+            self?.presentViewController(vc)
+          }
+        }
+      })
+      .disposed(by: disposeBag)
+  }
+}
+
+extension EnterRoomViewController {
+  private func presentViewController(_ viewController: UIViewController) {
+    viewController.modalTransitionStyle = .crossDissolve
+    viewController.modalPresentationStyle = .fullScreen
+    self.present(viewController, animated: true, completion: nil)
   }
 }
