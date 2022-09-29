9691dabc37d0c26f7163ee0e28d0ae1dd2094659
Jihyun247 <wlguslove12345@gmail.com>
2022-09-29 16:27:31 +0900

    [#15] FEAT : window rootVC 수정

--- Hous-iOS-release/Application/SceneDelegate.swift
+++ Hous-iOS-release/Application/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     guard let windowScene = (scene as? UIWindowScene) else { return }
     let window = UIWindow(windowScene: windowScene)
     window.backgroundColor = .white
-    window.rootViewController = UINavigationController(rootViewController: CreateNewRoomViewController())
+    window.rootViewController = HousTabbarViewController()
     window.makeKeyAndVisible()
 
     self.window = window
