e637a1cb195fad44ff28fc8d6a12f806dd7115db
psychehose <psychehose@gmail.com>
2022-09-30 23:01:36 +0900

    [#22] ADD - Splash Lottie view 구현

--- Hous-iOS-release/Scene/Splash/SplashViewController.swift
+++ Hous-iOS-release/Scene/Splash/SplashViewController.swift
@@ -5,18 +5,41 @@
 //  Created by 김호세 on 2022/08/13.
 //
 
+
+import Alamofire
+import Lottie
+import RxCocoa
+import RxSwift
+import SnapKit
 import UIKit
 
-import RxCocoa
-import Alamofire
-import RxSwift
-import Lottie
-
 final class SplashViewController: UIViewController {
+
+  private let lottieView: AnimationView = {
+    let view = AnimationView(name: "splashlottie")
+    view.contentMode = .scaleAspectFit
+    return view
+  }()
 
   private let disposeBag = DisposeBag()
 
   override func viewDidLoad() {
     super.viewDidLoad()
+
+    setupViews()
+  }
+
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    lottieView.play()
+  }
+
+  private func setupViews() {
+    view.addSubView(lottieView)
+
+    lottieView.snp.makeConstraints { make in
+      make.top.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+      make.bottom.equalToSuperview()
+    }
   }
 }
