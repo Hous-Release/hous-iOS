2a6dc61d9c94fd137d0559cd84fde220fe9db87b
psychehose <psychehose@gmail.com>
2022-10-03 19:37:24 +0900

    [#22] FIX: - parameter 값 변경

--- Network/Sources/Network/Model/Global/Token.swift
+++ Network/Sources/Network/Model/Global/Token.swift
@@ -8,6 +8,11 @@
 import Foundation
 
 public struct Token: Codable {
+  public init(accessToken: String, refreshToken: String) {
+    self.accessToken = accessToken
+    self.refreshToken = refreshToken
+  }
+
   public let accessToken: String
   public let refreshToken: String
 }
