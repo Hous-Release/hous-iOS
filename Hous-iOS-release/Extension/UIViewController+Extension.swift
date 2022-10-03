6d6fa7fc5df504d011af8df425c04e93872cf5e8
psychehose <psychehose@gmail.com>
2022-10-03 20:09:47 +0900

    [#22] ADD: Root VC 바꾸는 extension 추가

---
+++ Hous-iOS-release/Extension/UIViewController+Extension.swift
@@ -0,0 +1,26 @@
+//
+//  UIViewController+Extension.swift
+//  Hous-iOS-release
+//
+//  Created by 김호세 on 2022/10/03.
+//
+
+import UIKit
+
+extension UIViewController {
+
+  func changeRootViewController(to rootVC: UIViewController) {
+    self.view.window?.rootViewController?.dismiss(animated: false) {
+
+      rootVC.modalPresentationStyle = .fullScreen
+
+      guard
+        let sceneDelegate = UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate
+      else { return }
+
+      sceneDelegate.window?.rootViewController?.present(rootVC, animated: true, completion: {
+        sceneDelegate.window?.rootViewController = rootVC
+      })
+    }
+  }
+}
