a54e263ffbc5cd3651ea4d7bb0953fe1b88d5bb0
psychehose <psychehose@gmail.com>
2022-10-03 21:42:48 +0900

    [#22] ADD: NetworkKit Token Interceptor 값 변경

--- Network/Package.swift
+++ Network/Package.swift
@@ -16,6 +16,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/Alamofire/Alamofire.git", exact: "5.6.1"),
+        .package(name: "UserInformation", path: "../UserInformation")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -24,6 +25,7 @@ let package = Package(
             name: "Network",
             dependencies: [
                 "Alamofire",
+                .product(name: "UserInformation", package: "UserInformation")
             ]),
         .testTarget(
             name: "NetworkTests",
