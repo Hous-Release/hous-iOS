6608de230bd58cce2177cd5f8fc8a02311e4c4c2
psychehose <psychehose@gmail.com>
2022-10-04 23:29:00 +0900

    [#22] ADD: Asset 추가 및 SignInViewController 뷰 완성

--- Hous-iOS-release/Application/SceneDelegate.swift
+++ Hous-iOS-release/Application/SceneDelegate.swift
@@ -16,7 +16,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     guard let windowScene = (scene as? UIWindowScene) else { return }
     let window = UIWindow(windowScene: windowScene)
     window.backgroundColor = .white
-
     window.rootViewController = SplashViewController(SplashReactor())
     window.makeKeyAndVisible()
 
