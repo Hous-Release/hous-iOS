549693f0afd2cf242a6bf955902a3459958d4b65
lee-euiji <leeuj9663@naver.com>
2022-10-08 10:57:28 +0900

    [#12] FEAT: CollectionView 구성 완료

--- Network/Sources/Network/API/AuthAPI/AuthService 2.swift
+++ Network/Sources/Network/API/AuthAPI/AuthService 2.swift
@@ -1,40 +1,40 @@
+////
+////  AuthService.swift
+////
+////
+////  Created by 김호세 on 2022/09/17.
+////
 //
-//  AuthService.swift
-//  
+//import Foundation
+//import Alamofire
 //
-//  Created by 김호세 on 2022/09/17.
+//public enum AuthService {
+//  case login(_ dto: AuthDTO.Request.LoginRequestDTO)
+//}
 //
-
-import Foundation
-import Alamofire
-
-public enum AuthService {
-  case login(_ dto: AuthDTO.Request.LoginRequestDTO)
-}
-
-extension AuthService: TargetType {
-  public var baseURL: String {
-    return API.apiBaseURL
-  }
-
-  public var path: String {
-    switch self {
-    case .login:
-      return "auth/login"
-    }
-  }
-
-  public var method: HTTPMethod {
-    switch self {
-    case .login:
-      return .post
-    }
-  }
-
-  public var parameters: RequestParams {
-    switch self {
-    case let .login(dto):
-      return .body(dto)
-    }
-  }
-}
+//extension AuthService: TargetType {
+//  public var baseURL: String {
+//    return API.apiBaseURL
+//  }
+//
+//  public var path: String {
+//    switch self {
+//    case .login:
+//      return "auth/login"
+//    }
+//  }
+//
+//  public var method: HTTPMethod {
+//    switch self {
+//    case .login:
+//      return .post
+//    }
+//  }
+//
+//  public var parameters: RequestParams {
+//    switch self {
+//    case let .login(dto):
+//      return .body(dto)
+//    }
+//  }
+//}
