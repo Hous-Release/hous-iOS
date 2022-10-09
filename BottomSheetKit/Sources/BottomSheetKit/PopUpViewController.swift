0203d244d420fdee4699e41557c4c4592f12bc8d
psychehose <psychehose@gmail.com>
2022-10-10 02:33:56 +0900

    [#28] ADD: 모듈 - Popup View를 띄울 PopVC 생성

--- BottomSheetKit/Sources/BottomSheetKit/PopUpViewController.swift
+++ BottomSheetKit/Sources/BottomSheetKit/PopUpViewController.swift
@@ -7,18 +7,57 @@
 
 import UIKit
 import SnapKit
-import Then
 
+internal final class PopUpViewController: UIViewController {
 
-// TODO: Asset 모듈화 하고 나중에 디펜던시 적용하고 코드 수정
-public class PopUpViewController: UIViewController {
+  private enum Constant {
+    static let horizontalMargin: CGFloat = 40
+    static let height: CGFloat = 180
+  }
 
-  public init() {
+  private let rootView: UIView = {
+    let view = UIView()
+    view.backgroundColor = .black.withAlphaComponent(0.7)
+    return view
+  }()
+
+  private var popUpView: UIView
+  private let popUpType: PopUpType
+
+  internal init(popUpType: PopUpType) {
+    self.popUpType = popUpType
+
+    switch popUpType {
+    case .twoButton(let cancelText, let actionText):
+      self.popUpView = TwoButtonPopUpView(cancelText, actionText)
+
+    case .oneButtonWithImage(let actionText, let image):
+      popUpView = UIView()
+      break
+    }
+
     super.init(nibName: nil, bundle: nil)
+    setupViews()
   }
 
   required init?(coder: NSCoder) {
     fatalError("PopUpViewController is not implemented")
+  }
+
+  private func setupViews() {
+    view.addSubview(rootView)
+    rootView.addSubview(popUpView)
+    rootView.snp.makeConstraints { make in
+      make.edges.equalToSuperview()
+    }
+
+    popUpView.snp.makeConstraints { make in
+      make.leading.trailing.equalToSuperview().inset(Constant.horizontalMargin)
+      make.centerY.equalToSuperview()
+
+      // TODO: - Change
+      make.height.equalTo(180)
+    }
   }
 
 }
