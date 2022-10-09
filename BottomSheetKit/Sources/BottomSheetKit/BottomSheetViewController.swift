2b2f606ba87e692f282becb4775cff062eb57c67
psychehose <psychehose@gmail.com>
2022-10-10 01:33:24 +0900

    [#28] ADD: 모듈 -BottomSheetViewController 작성

--- BottomSheetKit/Sources/BottomSheetKit/BottomSheetViewController.swift
+++ BottomSheetKit/Sources/BottomSheetKit/BottomSheetViewController.swift
@@ -1,8 +1,21 @@
 //
-//  File.swift
+//  BottomSheetViewController.swift
 //  
 //
 //  Created by 김호세 on 2022/10/10.
 //
 
-import Foundation
+import SnapKit
+import UIKit
+
+public final class BottomSheetViewController: UIViewController {
+
+  public init() {
+    super.init(nibName: nil, bundle: nil)
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("BottomSheetViewController is not implemented")
+  }
+
+}
