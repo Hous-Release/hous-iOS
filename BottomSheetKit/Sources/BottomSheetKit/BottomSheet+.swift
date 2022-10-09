e4fb8492757e16b0f84e87e03975f403aaa97ad8
psychehose <psychehose@gmail.com>
2022-10-10 02:34:49 +0900

    [#28] ADD: 모듈 - APP에서 노출할 presentPopUp 함수 생성

--- BottomSheetKit/Sources/BottomSheetKit/BottomSheet+.swift
+++ BottomSheetKit/Sources/BottomSheetKit/BottomSheet+.swift
@@ -9,4 +9,9 @@ import UIKit
 
 public extension UIViewController {
 
+  func presentPopUp(_ type: PopUpType) {
+    let popupVC = PopUpViewController(popUpType: type)
+    popupVC.modalPresentationStyle = .overFullScreen
+    self.present(popupVC, animated: true)
+  }
 }
