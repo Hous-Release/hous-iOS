4080788e12ebe8bdd1d35b84326dde5ee0f02c20
psychehose <psychehose@gmail.com>
2022-10-10 02:31:19 +0900

    [#28] ADD: 모듈 - Popup Model 정의

--- BottomSheetKit/Sources/BottomSheetKit/Model/PopUpModel.swift
+++ BottomSheetKit/Sources/BottomSheetKit/Model/PopUpModel.swift
@@ -1,8 +1,25 @@
 //
-//  File.swift
+//  PopUpModel.swift
 //  
 //
 //  Created by 김호세 on 2022/10/10.
 //
 
 import Foundation
+import UIKit
+
+public struct PopUpModel {
+  public let popUpType: PopUpType
+
+  public init(popUpType: PopUpType) {
+    self.popUpType = popUpType
+  }
+}
+
+public enum PopUpType {
+  case twoButton(cancelText: String, actionText: String)
+  case oneButtonWithImage(actionText: String, image: UIImage)
+}
+
+// TODO: TwoButton Model 만들기
+// TODO: OneButton Image 만들기
