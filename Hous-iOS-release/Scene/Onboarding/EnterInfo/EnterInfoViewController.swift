dafa5e442d37c27d5448819e9778e54c5e2b0305
Kim Minjae <ffalswo2@naver.com>
2022-10-13 22:42:17 +0900

    [#21] CHORE: 코드 수정

--- Hous-iOS-release/Scene/Onboarding/EnterInfo/EnterInfoViewController.swift
+++ Hous-iOS-release/Scene/Onboarding/EnterInfo/EnterInfoViewController.swift
@@ -106,6 +106,10 @@ extension EnterInfoViewController {
 }
 
 extension EnterInfoViewController: NavBarWithBackButtonViewDelegate {
+  func backButtonDidTappedWithoutPopUp() {
+    print("back")
+  }
+  
   func backButtonDidTapped() {
     navigationController?.popViewController(animated: true)
   }
