c24e1bf33bbd0d3c0586441e386aa47a89b49ca5
Jihyun247 <wlguslove12345@gmail.com>
2022-10-21 12:45:56 +0900

    [#32] FEAT: Auth Signup Request & Response DTO + API Service

--- Network/Sources/Network/API/AuthAPI/AuthAPI.swift
+++ Network/Sources/Network/API/AuthAPI/AuthAPI.swift
@@ -48,7 +48,9 @@ public final class AuthAPI: APIRequestLoader<AuthService>, AuthAPIProtocol {
       }
   }
 
-  public func signup(_ signupReauestDTO: AuthDTO.Request.SignupRequestDTO, completion: @escaping (BaseResponseType<AuthDTO.Response.SignupResponseDTO>?, Error?) -> Void) {
+  public func signup(
+    _ signupReauestDTO: AuthDTO.Request.SignupRequestDTO,
+    completion: @escaping (BaseResponseType<AuthDTO.Response.SignupResponseDTO>?, Error?) -> Void) {
     fetchData(
       target: .signup(signupReauestDTO),
       responseData: BaseResponseType<AuthDTO.Response.SignupResponseDTO>.self,
