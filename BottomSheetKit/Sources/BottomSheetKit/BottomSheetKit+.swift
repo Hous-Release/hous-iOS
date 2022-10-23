1af62b88d75030301bd99f04f592a07c788edd15
psychehose <psychehose@gmail.com>
2022-10-23 22:19:43 +0900

    [#28] ADD: Default PopUp Action 구현 ( SRP, OOP)

---
+++ BottomSheetKit/Sources/BottomSheetKit/BottomSheetKit+.swift
@@ -0,0 +1,36 @@
+//
+//  BottomSheet+.swift
+//  
+//
+//  Created by 김호세 on 2022/10/09.
+//
+
+import UIKit
+
+public extension UIViewController {
+
+  func presentPopUp(
+    _ type: PopUpType,
+    _ completion: CompleteAction? = nil
+  ) {
+
+    switch type {
+    case .twoButton(let twoButtonPopUpModel):
+
+      let view = DefaultPopUpView(twoButtonPopUpModel)
+      let popUpAction = DefaultPopUpAction(view: view)
+      let vc = DefaultPopUpViewController(popUpAction: popUpAction)
+
+      popUpAction.completeAction = { actionType in
+        completion?(actionType)
+      }
+
+      vc.modalTransitionStyle = .crossDissolve
+      vc.modalPresentationStyle = .overFullScreen
+      present(vc, animated: true)
+
+    case .oneButtonWithImage(let actionText, let image):
+      break
+    }
+  }
+}
---
+++ BottomSheetKit/Sources/BottomSheetKit/Model/DidActionType.swift
@@ -0,0 +1,13 @@
+//
+//  PopUpActionType.swift
+//  
+//
+//  Created by 김호세 on 2022/10/23.
+//
+
+import Foundation
+
+public enum DidActionType {
+  case cancel
+  case action
+}
