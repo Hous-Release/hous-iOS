0e43e2dd5e03fb7af73e688cbfc5857d345c1dda
Kim Minjae <ffalswo2@naver.com>
2022-10-26 22:08:34 +0900

    [#39] ADD : OurRuleViewController 추가

---
+++ Hous-iOS-release/Scene/OurRule/OurRulesViewController.swift
@@ -0,0 +1,21 @@
+//
+//  OurRulesViewController.swift
+//  Hous-iOS-release
+//
+//  Created by 김민재 on 2022/10/26.
+//
+
+import UIKit
+
+class OurRulesViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  
+  
+  
+  
+  
+}
