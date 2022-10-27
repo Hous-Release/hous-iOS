f2cc6071ad27f8bc7fd5324cd6bfec71fa9cc196
Kim Minjae <ffalswo2@naver.com>
2022-10-27 21:54:19 +0900

    [#39] ADD : OurRules뷰 TableView Cells 추가

---
+++ Hous-iOS-release/Scene/OurRule/Cells/KeyRulesTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  KeyRulesTableViewCell.swift
+//  Hous-iOS-release
+//
+//  Created by 김민재 on 2022/10/27.
+//
+
+import UIKit
+
+class KeyRulesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
