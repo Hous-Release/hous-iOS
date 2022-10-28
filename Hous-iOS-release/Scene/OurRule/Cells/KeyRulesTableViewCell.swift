1a64dfa0b467d9961217096d42b646ff1a126b5e
Kim Minjae <ffalswo2@naver.com>
2022-10-28 18:57:39 +0900

    [#39] FEAT : Rules 기본 뷰 레이아웃 구현

--- Hous-iOS-release/Scene/OurRule/Cells/KeyRulesTableViewCell.swift
+++ Hous-iOS-release/Scene/OurRule/Cells/KeyRulesTableViewCell.swift
@@ -8,16 +8,83 @@
 import UIKit
 
 class KeyRulesTableViewCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+  
+  private let KeyRulesBackgroundView = UIView().then {
+    $0.backgroundColor = Colors.blueL2.color
+    $0.layer.cornerRadius = 8
+  }
+  
+  private let ruleEmptyViewLabel = UILabel().then {
+    $0.isHidden = true
+    $0.text = "아직 우리 집 Rules가 없어요!"
+    $0.font = Fonts.SpoqaHanSansNeo.medium.font(size: 14)
+    $0.textColor = Colors.g5.color
+  }
+  
+  private let ourRulesStackView = UIStackView(arrangedSubviews: []).then {
+    $0.axis = .vertical
+    $0.backgroundColor = Colors.blueL1.color
+    $0.spacing = 1
+    $0.distribution = .fillEqually
+  }
+  
+  override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+    super.init(style: style, reuseIdentifier: reuseIdentifier)
+    configUI()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  override func setSelected(_ selected: Bool, animated: Bool) {
+    super.setSelected(selected, animated: animated)
+    
+  }
+  
+  private func configUI() {
+    addSubView(KeyRulesBackgroundView)
+    
+    KeyRulesBackgroundView.addSubViews([ourRulesStackView,ruleEmptyViewLabel]
+    )
+    
+    KeyRulesBackgroundView.snp.makeConstraints { make in
+      make.top.bottom.equalToSuperview()
+      make.leading.trailing.equalToSuperview().inset(24)
+      make.width.equalTo(UIScreen.main.bounds.width * (327/375))
+      make.height.equalTo(KeyRulesBackgroundView.snp.width).multipliedBy(0.4770642202)
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    ourRulesStackView.snp.makeConstraints { make in
+      make.edges.equalToSuperview().inset(20)
     }
-
+    
+    ruleEmptyViewLabel.snp.makeConstraints { make in
+      make.center.equalToSuperview()
+    }
+  }
+  
+  func setKeyRulesCell(ourRules: [String]) {
+    if ourRules.count == 0 {
+      ourRulesStackView.isHidden = true
+      ruleEmptyViewLabel.isHidden = false
+      return
+    }
+    
+    ourRulesStackView.subviews.forEach { $0.removeFromSuperview() }
+    ourRules.enumerated().forEach { (idx, rule) in
+      let label = OurRulesView()
+      label.setRulesCell(number: idx+1, ruleText: rule)
+      ourRulesStackView.addArrangedSubview(label)
+    }
+    if ourRules.count < 3 {
+      let diff = 3 - ourRules.count
+      for i in 0..<diff {
+        let label = OurRulesView()
+        label.setEmptyRule(number: diff+i)
+        ourRulesStackView.addArrangedSubview(label)
+      }
+    }
+    
+  }
 }
--- Hous-iOS-release/Scene/OurRule/OurRulesViewController.swift
+++ Hous-iOS-release/Scene/OurRule/OurRulesViewController.swift
@@ -6,16 +6,80 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
 
 class OurRulesViewController: UIViewController {
   
+  private let navigationBar: NavBarWithBackButtonView = {
+    let navBar = NavBarWithBackButtonView(title: "우리 집 Rules")
+    navBar.setRightButtonImage(image: Images.frame1.image)
+    return navBar
+  }()
+  
+  private let rulesTableView = UITableView()
+  
+  let disposeBag = DisposeBag()
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+    configUI()
+    setTableView()
+    bind(RulesViewModel(dummy: ["규칙추가1", "규칙추가2", "규칙추가3", "규칙 ㅁㄴ아ㅜ"]))
   }
   
+  private func setTableView() {
+    rulesTableView.register(KeyRulesTableViewCell.self, forCellReuseIdentifier: KeyRulesTableViewCell.className)
+    
+    rulesTableView.register(RulesTableViewCell.self, forCellReuseIdentifier: RulesTableViewCell.className)
+    
+    rulesTableView.rowHeight = UITableView.automaticDimension
+    rulesTableView.estimatedRowHeight = 150
+  }
   
+  private func configUI() {
+    self.view.addSubViews([
+      navigationBar,
+      rulesTableView
+    ])
+    
+    navigationBar.snp.makeConstraints { make in
+      make.top.equalTo(view.safeAreaLayoutGuide)
+      make.leading.trailing.equalToSuperview()
+      make.height.equalTo(60)
+    }
+    
+    rulesTableView.snp.makeConstraints { make in
+      make.top.equalTo(navigationBar.snp.bottom)
+      make.leading.trailing.equalToSuperview()
+      make.bottom.equalTo(self.view.safeAreaLayoutGuide)
+    }
+  }
   
-  
-  
-  
+  private func bind(_ viewModel: RulesViewModel) {
+    viewModel.getCellData()
+      .bind(to: rulesTableView.rx.items) { (tableView, row, item) -> UITableViewCell in
+        if row == 0 {
+          guard let cell = tableView.dequeueReusableCell(withIdentifier: KeyRulesTableViewCell.className, for: IndexPath.init(row: row, section: 0)) as? KeyRulesTableViewCell else {
+            return UITableViewCell()
+          }
+          cell.setKeyRulesCell(ourRules: [item])
+          cell.selectionStyle = .none
+          cell.separatorInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: tableView.bounds.width)
+          return cell
+        }
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: RulesTableViewCell.className, for: IndexPath.init(row: row, section: 0)) as? RulesTableViewCell else {
+          return UITableViewCell()
+        }
+        
+        cell.separatorInset = UIEdgeInsets(top: 0, left: 24, bottom: 0, right: 24)
+        cell.setNormalRulesData(rule: item)
+        cell.selectionStyle = .none
+        return cell
+        
+      }
+      .disposed(by: disposeBag)
+  }
 }
