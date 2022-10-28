0fcc6f3e4bb9dd4666ccd0d190c4e8b62239aaaf
Kim Minjae <ffalswo2@naver.com>
2022-10-28 18:57:39 +0900

    [#39] FEAT : Rules 기본 뷰 레이아웃 구현

--- Hous-iOS-release/Scene/OurRule/RulesViewModel.swift
+++ Hous-iOS-release/Scene/OurRule/RulesViewModel.swift
@@ -1,8 +1,21 @@
 //
-//  RuleCellViewModel.swift
+//  RulesViewModel.swift
 //  Hous-iOS-release
 //
 //  Created by 김민재 on 2022/10/28.
 //
 
 import Foundation
+import RxSwift
+
+class RulesViewModel {
+  var dummy: [String]
+  
+  init(dummy: [String]) {
+    self.dummy = dummy
+  }
+  
+  func getCellData() -> Observable<[String]> {
+    return Observable.of(dummy)
+  }
+}
