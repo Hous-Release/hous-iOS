c6e23df1ce81fae36e425b9fe47a18f6a890c3b3
Kim Minjae <ffalswo2@naver.com>
2022-10-29 16:23:39 +0900

    [#39] FEAT : Rule 네트워크 세팅

---
+++ Network/Sources/Network/Model/Rule/RuleResponseDTO.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  
+//
+//  Created by 김민재 on 2022/10/28.
+//
+
+public extension RuleDTO.Response {
+  struct RuleResponseDTO: Decodable {
+    public let rules: [Rule]
+  }
+  
+  struct Rule: Decodable {
+    public let id: Int
+    public let name: String
+  }
+}
+
