1bbac71fc7ab743027c78d0b9bd12e19a6165b13
lee-euiji <leeuj9663@naver.com>
2022-10-30 01:39:35 +0900

    [#37] FEAT: Detail Cell 화면 연결 + 레이아웃 구성 완료

--- Hous-iOS-release/Scene/Profile/Cell/ProfileDetailRecommendCollectionViewCell.swift
+++ Hous-iOS-release/Scene/Profile/Cell/ProfileDetailRecommendCollectionViewCell.swift
@@ -8,12 +8,56 @@
 import UIKit
 
 final class ProfileDetailRecommendCollectionViewCell: UICollectionViewCell {
-  override init(frame: CGRect) {
-    super.init(frame: frame)
-    self.backgroundColor = .green
+  private enum Size{
+    static let screenWidth = UIScreen.main.bounds.width
+    static let screenHeight = UIScreen.main.bounds.height
+    static let stackViewHeight = 202
   }
   
-  required init?(coder: NSCoder) {
+  private let profileTestResultRecommendStackView = UIStackView().then {
+    $0.distribution = .fillProportionally
+    $0.alignment = .fill
+    $0.axis = .horizontal
+    $0.spacing = 4
+  }
+  
+  private var badRecommendView = ProfileRecommendBoxView(personalityColor: .blue, cellType: .bad)
+  
+  private var goodRecommendView = ProfileRecommendBoxView(personalityColor: .red, cellType: .good)
+  
+  override init(frame: CGRect){
+    super.init(frame: frame)
+    configUI()
+    render()
+  }
+  
+  required init?(coder: NSCoder){
     fatalError("init(coder:) has not been implemented")
   }
+  
+  private func configUI(){
+    self.backgroundColor = .white
+  }
+  
+  private func render(){
+
+    profileTestResultRecommendStackView.addArrangedSubviews(badRecommendView, goodRecommendView)
+    self.addSubView(profileTestResultRecommendStackView)
+    profileTestResultRecommendStackView.snp.makeConstraints {make in
+      make.leading.trailing.equalToSuperview().inset(24)
+      make.top.equalToSuperview().offset(2)
+      make.height.equalTo(Size.stackViewHeight)
+    }
+  }
+  
+//  func setData(_ dataPack: ProfileTestResultDataPack) {
+//    self.badRecommendView.backgroundColor = dataPack.badPersonalityType.backgroundColor
+//    self.badRecommendView.titleLabel.textColor = dataPack.badPersonalityType.textColor
+//    self.badRecommendView.personalityLabel.text = dataPack.badPersonalityLabel
+//    self.badRecommendView.personalityImageView.urlToImage(urlString: dataPack.badPersonalityImageURL)
+//    self.goodRecommmendView.backgroundColor = dataPack.goodPersonalityType.backgroundColor
+//    self.goodRecommmendView.titleLabel.textColor = dataPack.goodPersonalityType.textColor
+//    self.goodRecommmendView.personalityLabel.text = dataPack.goodPersonalityLabel
+//    self.goodRecommmendView.personalityImageView.urlToImage(urlString: dataPack.goodPersonalityImageURL)
+//  }
 }
--- Hous-iOS-release/Scene/Profile/Cell/ProfileDetailTextCollectionViewCell.swift
+++ Hous-iOS-release/Scene/Profile/Cell/ProfileDetailTextCollectionViewCell.swift
@@ -8,14 +8,76 @@
 import UIKit
 
 final class ProfileDetailTextCollectionViewCell: UICollectionViewCell {
-  
-  override init(frame: CGRect) {
-    super.init(frame: frame)
-    self.backgroundColor = .red
+
+  private enum Size{
+    static let screenWidth = UIScreen.main.bounds.width
+    static let screenHeight = UIScreen.main.bounds.height
+    static let recommendRuleTitleViewSize = CGSize(width: Size.screenWidth - 48, height: 34)
+    static let recommendRuleViewSIze = CGSize(width: Size.screenWidth - 48, height: 86)
   }
   
-  required init?(coder: NSCoder) {
+  private let descriptionView = ProfileDescriptionView()
+  
+  private let recommendRuleTitleView = ProfileRecommendTitleView()
+  
+  private let recommendRuleView = ProfileRecommendRuleView()
+  
+  override init(frame: CGRect){
+    super.init(frame: frame)
+    configUI()
+    render()
+  }
+  
+  required init?(coder: NSCoder){
     fatalError("init(coder:) has not been implemented")
   }
+  
+  private func configUI(){
+    self.backgroundColor = .white
+  }
+  
+  private func render() {
+    self.addSubViews([descriptionView, recommendRuleTitleView, recommendRuleView])
+    
+    descriptionView.snp.makeConstraints {make in
+      make.centerX.equalToSuperview()
+      make.top.equalToSuperview()
+      make.width.equalTo(Size.screenWidth - 48)
+    }
+    
+    recommendRuleTitleView.snp.makeConstraints {make in
+      make.centerX.equalToSuperview()
+      make.top.equalTo(descriptionView.snp.bottom).offset(18)
+      make.width.height.equalTo(Size.recommendRuleTitleViewSize)
+    }
+    
+    recommendRuleView.snp.makeConstraints {make in
+      make.centerX.equalToSuperview()
+      make.top.equalTo(recommendRuleTitleView.snp.bottom)
+      make.width.height.equalTo(Size.recommendRuleViewSIze)
+    }
+  }
+  
+//  func setData(_ dataPack: ProfileTestResultDataPack) {
+//    self.descriptionView.personalityTitleLabel.text = dataPack.personalityTitleLabel
+//    self.descriptionView.personalityTitleLabel.textColor = dataPack.personalityType.textColor
+//
+//    let descriptionAttributedString = NSAttributedString(string: dataPack.personalityDescriptionLabel).withLineSpacing(2.5)
+//    self.descriptionView.personalityDescriptionLabel.attributedText = descriptionAttributedString
+//
+//    self.descriptionView.backgroundColor = dataPack.personalityType.backgroundColor
+//
+//    self.recommendRuleTitleView.recommendTitleLabel.text = dataPack.recommandTitleLabel
+//    self.recommendRuleTitleView.recommendTitleLabel.textColor = dataPack.personalityType.textColor
+//    self.recommendRuleTitleView.backgroundColor = dataPack.personalityType.backgroundColor
+//
+//    for (index, item) in dataPack.recommandRuleLabel.enumerated() {
+//      self.recommendRuleView.recommendRuleStackItems[index].setLabelText(item)
+//    }
+//
+//    self.recommendRuleView.backgroundColor = dataPack.personalityType.backgroundColor
+//    self.recommendRuleView.recommendRuleStackItems[0].indexImageView.image = dataPack.personalityType.ruleRecommendImage
+//    self.recommendRuleView.recommendRuleStackItems[1].indexImageView.image = dataPack.personalityType.ruleRecommendImage
+    
 }
 
