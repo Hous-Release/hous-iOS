af0d46d7cd5a3f7d92114f6763ac2741211e57ea
Kim Minjae <ffalswo2@naver.com>
2022-10-30 23:19:17 +0900

    [#39] FEAT : 규칙뷰 기본 뷰 레이아웃 및 네트워크 구현

--- Network/Sources/Network/API/RuleAPI/RuleAPI.swift
+++ Network/Sources/Network/API/RuleAPI/RuleAPI.swift
@@ -14,18 +14,18 @@ protocol RuleAPIProtocol {
 
 public final class RuleAPI: APIRequestLoader<RuleService>, RuleAPIProtocol {
     public func getRulesName() -> Observable<[RuleDTO.Response.Rule]> {
-        print("getRulesName 호출됨 ! ")
+        
         return Observable.create { [weak self] emitter in
             
-            self?.fetchData(target: .getRuleData,
-                      responseData: BaseResponseType<RuleDTO.Response.RuleResponseDTO>.self) { result, error in
-                
+            self?.fetchData(
+                target: .getRuleData,
+                responseData: BaseResponseType<RuleDTO.Response.RuleResponseDTO>.self
+            ) { result, error in
                 if let error = error {
                     emitter.onError(error)
                 }
+                
                 if let result = result {
-                    print("=============")
-                    print(result)
                     emitter.onNext(result.data?.rules ?? [])
                     emitter.onCompleted()
                 }
