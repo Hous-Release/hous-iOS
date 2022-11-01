881e41559f40fa1ff765121d6cf4bdf4b2199dee
Kim Minjae <ffalswo2@naver.com>
2022-11-02 04:28:26 +0900

    [#43] ADD : 규칙 수정뷰 관련 파일 생성
    - ViewController, TableViewCell, ViewModel

---
+++ Hous-iOS-release/Scene/OurRule/EditDeleteRuleView/EditRuleTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  EditRuleTableViewCell.swift
+//  Hous-iOS-release
+//
+//  Created by 김민재 on 2022/11/01.
+//
+
+import UIKit
+
+class EditRuleTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
