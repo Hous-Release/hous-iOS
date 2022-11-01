c6e1521c40874791132c109ba0cfcafbc250239f
Kim Minjae <ffalswo2@naver.com>
2022-11-02 06:11:43 +0900

    [#43] FEAT : 규칙 수정 네트워크 세팅

--- Network/Sources/Network/Model/Rule/RuleRequestDTO.swift
+++ Network/Sources/Network/Model/Rule/RuleRequestDTO.swift
@@ -8,38 +8,39 @@
 import Foundation
 
 public extension RuleDTO.Request {
-  struct updateRuleRequestDTO: Encodable {
-    
-    public let name: String
-    
-    public init(name: String) {
-      self.name = name
+    struct createRuleRequestDTO: Encodable {
+        public let ruleNames: [String]
+        
+        public init(ruleNames: [String]) {
+            self.ruleNames = ruleNames
+        }
     }
-  }
-  
-  struct createRuleRequestDTO: Encodable {
-    public let ruleNames: [String]
     
-    public init(ruleNames: [String]) {
-      self.ruleNames = ruleNames
+    struct updateRulesRequestDTO: Encodable {
+        public let rules: [Rule]
+        
+        public init(rules: [Rule]) {
+            self.rules = rules
+        }
     }
-  }
-  
-  struct updateRulesOrderRequestDTO: Encodable {
-    public let rulesIdList: [Int]
     
-    public init(rulesIdList: [Int]) {
-      self.rulesIdList = rulesIdList
+    struct Rule: Encodable {
+        public let id: Int
+        public let name: String
+        
+        public init(id: Int, name: String) {
+            self.id = id
+            self.name = name
+        }
     }
-  }
-  
-  /// 삭제할 규칙들의 idList
-  struct deleteRulesRequestDTO: Encodable {
-    public let rulesIdList: [Int]
     
-    public init(rulesIdList: [Int]) {
-      self.rulesIdList = rulesIdList
+    /// 삭제할 규칙들의 idList
+    struct deleteRulesRequestDTO: Encodable {
+        public let rulesIdList: [Int]
+        
+        public init(rulesIdList: [Int]) {
+            self.rulesIdList = rulesIdList
+        }
     }
-  }
-  
+    
 }
