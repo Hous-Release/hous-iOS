41e1fb4ddcb5e27679e6ea31ace0c16059e6cf54
Kim Minjae <ffalswo2@naver.com>
2022-11-02 04:28:26 +0900

    [#43] ADD : 규칙 수정뷰 관련 파일 생성
    - ViewController, TableViewCell, ViewModel

---
+++ Hous-iOS-release/Scene/OurRule/EditDeleteRuleView/EditRuleViewController.swift
@@ -0,0 +1,29 @@
+//
+//  EditRuleViewController.swift
+//  Hous-iOS-release
+//
+//  Created by 김민재 on 2022/11/01.
+//
+
+import UIKit
+
+class EditRuleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
