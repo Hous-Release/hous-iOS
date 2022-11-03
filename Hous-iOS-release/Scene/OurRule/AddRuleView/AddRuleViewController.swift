4aa1adef7862c2be905fd0b6ffe65a2bda6c3dff
Kim Minjae <ffalswo2@naver.com>
2022-11-04 01:47:34 +0900

    [#50] ADD : AddRuleVC, AddRuleVM 파일 추가

---
+++ Hous-iOS-release/Scene/OurRule/AddRuleView/AddRuleViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AddRuleViewController.swift
+//  Hous-iOS-release
+//
+//  Created by 김민재 on 2022/11/04.
+//
+
+import UIKit
+
+class AddRuleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Hous-iOS-release/Scene/OurRule/AddRuleView/AddRuleViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  AddRuleViewModel.swift
+//  Hous-iOS-release
+//
+//  Created by 김민재 on 2022/11/04.
+//
+
+import Foundation
