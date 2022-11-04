ada571d9126470dfc3ff92c9453a294f3eaafebb
Kim Minjae <ffalswo2@naver.com>
2022-11-04 21:34:14 +0900

    [#54] ADD : DeleteRuleViewVC, DeleteRuleVM 파일 추가

---
+++ Hous-iOS-release/Scene/OurRule/DeleteRuleView/DeleteRuleViewController.swift
@@ -0,0 +1,33 @@
+//
+//  DeleteRuleViewController.swift
+//  Hous-iOS-release
+//
+//  Created by 김민재 on 2022/11/04.
+//
+
+import UIKit
+
+class DeleteRuleViewController: UIViewController {
+  //MARK: - UI Components
+  
+  
+  //MARK: - var & let
+  
+  
+  //MARK: - Lifecycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    configUI()
+    bind()
+  }
+  
+  //MARK: - Custom Methods
+  private func configUI() {
+    
+  }
+  
+  private func bind() {
+    
+  }
+  
+}
