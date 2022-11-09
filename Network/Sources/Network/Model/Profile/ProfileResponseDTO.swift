652ff2d4584da7d6a8f1bfd83cf49ea7c7472a57
Kim Minjae <ffalswo2@naver.com>
2022-11-10 06:52:28 +0900

    [#68] FEAT : 배지정보조회 GET API 구현

--- Network/Sources/Network/Model/Profile/ProfileResponseDTO.swift
+++ Network/Sources/Network/Model/Profile/ProfileResponseDTO.swift
@@ -3,7 +3,7 @@
 //  
 //
 //  Created by 이의진 on 2022/10/30.
-//
+//  Created by 김민재 on 2022/11/08.
 
 
 import Foundation
@@ -23,4 +23,40 @@ public extension ProfileDTO.Response {
         public let clean, introversion, light, noise, smell: Double
     }
     
+    
+    // MARK: - GET Badge
+    struct getBadgesResponseDTO: Decodable {
+        public let badges: [Badge]
+        public let representBadge: RepresentBadge?
+    }
+    
+    // MARK: - Badge
+    struct Badge: Decodable {
+        public let badgeID: Int
+        public let badgeDescription: String
+        public let imageURL: String
+        public let isAcquired: Bool
+        public let isRead: Bool
+        public let name: String
+
+        enum CodingKeys: String, CodingKey {
+            case badgeID = "badgeId"
+            case badgeDescription = "description"
+            case imageURL = "imageUrl"
+            case isAcquired, isRead, name
+        }
+    }
+
+    // MARK: - RepresentBadge
+    struct RepresentBadge: Decodable {
+        public let badgeID: Int
+        public let imageURL: String
+        public let name: String
+
+        enum CodingKeys: String, CodingKey {
+            case badgeID = "badgeId"
+            case imageURL = "imageUrl"
+            case name
+        }
+    }
 }
