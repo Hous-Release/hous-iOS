925df7be7f5de1e743a1d49df033c66d486f8d44
Jihyun247 <wlguslove12345@gmail.com>
2022-11-13 21:29:45 +0900

    [#73] FEAT: Todo Main Todo 체크 서버통신

--- Network/Sources/Network/API/OnboardingAPI/OnboardingService.swift
+++ Network/Sources/Network/API/OnboardingAPI/OnboardingService.swift
@@ -10,44 +10,44 @@ import Alamofire
 
 public enum OnboardingService {
     case postNewRoom(_ name: OnboardingDTO.Request.PostNewRoomRequestDTO)
-  case postExistRoom(_ roomId: Int)
+    case postExistRoom(_ roomId: Int)
     case getIsExistRoom(_ code: OnboardingDTO.Request.GetIsExistRoomRequestDTO)
 }
 
 extension OnboardingService: TargetType {
-  public var baseURL: String {
-    return API.apiBaseURL
-  }
-
-  public var path: String {
-    switch self {
-
-    case .postNewRoom:
-      return "/room"
-    case let .postExistRoom(roomId):
-      return "/room/\(roomId)/join"
-    case .getIsExistRoom:
-      return "/room/info"
+    public var baseURL: String {
+        return API.apiBaseURL
     }
-  }
 
-  public var method: HTTPMethod {
-    switch self {
-    case .postNewRoom, .postExistRoom:
-      return .post
-    case .getIsExistRoom:
-      return .get
-    }
-  }
+    public var path: String {
+        switch self {
 
-  public var parameters: RequestParams {
-    switch self {
-    case let .postNewRoom(name):
-      return .body(name)
-    case .postExistRoom:
-      return .requestPlain
-    case let .getIsExistRoom(code):
-      return .query(code)
+        case .postNewRoom:
+            return "/room"
+        case let .postExistRoom(roomId):
+            return "/room/\(roomId)/join"
+        case .getIsExistRoom:
+            return "/room/info"
+        }
     }
-  }
+
+    public var method: HTTPMethod {
+        switch self {
+        case .postNewRoom, .postExistRoom:
+            return .post
+        case .getIsExistRoom:
+            return .get
+        }
+    }
+
+    public var parameters: RequestParams {
+        switch self {
+        case let .postNewRoom(name):
+            return .body(name)
+        case .postExistRoom:
+            return .requestPlain
+        case let .getIsExistRoom(code):
+            return .query(code)
+        }
+    }
 }
