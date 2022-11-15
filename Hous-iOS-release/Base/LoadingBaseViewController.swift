303a96cde20112335550c9ddd69e28d2adfc16a1
Kim Minjae <ffalswo2@naver.com>
2022-11-16 01:29:16 +0900

    [#79] ADD : LoadingBaseVC 파일 추가

---
+++ Hous-iOS-release/Base/LoadingBaseViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LoadingBaseViewController.swift
+//  Hous-iOS-release
+//
+//  Created by 김민재 on 2022/11/16.
+//
+
+import UIKit
+
+class LoadingBaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
