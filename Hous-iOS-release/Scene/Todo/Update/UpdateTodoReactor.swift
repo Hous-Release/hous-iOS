b1d3aa88fa1ff0ba290e6d6a0ff982c25b7b130c
psychehose <psychehose@gmail.com>
2022-11-20 15:34:47 +0900

    [#77] Modify: Reactor, Repository public

--- Hous-iOS-release/Scene/Todo/Update/UpdateTodoReactor.swift
+++ Hous-iOS-release/Scene/Todo/Update/UpdateTodoReactor.swift
@@ -8,11 +8,11 @@
 import Foundation
 import ReactorKit
 
-final class UpdateTodoReactor: Reactor {
+public final class UpdateTodoReactor: Reactor {
   private let provider: ServiceProvider
-  internal let initialState: State
+  public let initialState: State
 
-  init(
+  public init(
     provider: ServiceProvider,
     state: State
   ) {
@@ -20,17 +20,57 @@ final class UpdateTodoReactor: Reactor {
     self.provider = provider
   }
 
+  public enum Action {
+    case fetch
+    case enterTodo
+    case didTapHomie(IndexPath)
+    case didTapDays(IndexPath)
+    case didTapUpdate
+  }
 
-  enum Action {
+  public enum Mutation {
 
   }
 
-  enum Mutation {
-
+  public struct State {
+    var isModifying: Bool = false
+    var isPushNotification: Bool = false
+    var todo: String? = nil
+    var todoHomies: [UpdateTodoHomieModel] = []
   }
 
-  struct State {
-
+  public func mutate(action: Action) -> Observable<Mutation> {
+    switch action {
+    case .fetch:
+      return .empty()
+    case .enterTodo:
+      return .empty()
+    case .didTapHomie:
+      return .empty()
+    case .didTapDays:
+      return .empty()
+    case .didTapUpdate:
+      return .empty()
+    }
   }
 
+  public func reduce(state: State, mutation: Mutation) -> State {
+    var newState = state
+
+    return newState
+  }
+
+}
+
+public extension UpdateTodoReactor {
+
+  func transform(mutation: Observable<Mutation>) -> Observable<Mutation> {
+    let service = provider.todoRepository.event.flatMap { [weak self] event -> Observable<Mutation> in
+      switch event {
+      default:
+        return .empty()
+      }
+    }
+    return Observable.merge(mutation, service)
+  }
 }
