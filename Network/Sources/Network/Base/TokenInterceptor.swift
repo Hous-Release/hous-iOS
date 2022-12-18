f6a7a4ae4509ebace80fa0db33fbd4bb57a38530
psychehose <psychehose@gmail.com>
2022-12-17 21:47:02 -0300

    [#141] ADD: Refresh 로직 추가

--- Network/Sources/Network/Base/TokenInterceptor.swift
+++ Network/Sources/Network/Base/TokenInterceptor.swift
@@ -10,7 +10,11 @@ import Alamofire
 import UserInformation
 
 internal final class TokenInterceptor: RequestInterceptor, RequestRetrier {
-  func adapt(_ urlRequest: URLRequest, for session: Session, completion: @escaping (Result<URLRequest, Error>) -> Void) {
+  func adapt(
+    _ urlRequest: URLRequest,
+    for session: Session,
+    completion: @escaping (Result<URLRequest, Error>) -> Void
+  ) {
     var urlRequest = urlRequest
 
     let value = Keychain.shared.getAccessToken() ?? ""
@@ -18,13 +22,39 @@ internal final class TokenInterceptor: RequestInterceptor, RequestRetrier {
     completion(.success(urlRequest))
   }
 
-  func retry(_ request: Request, for session: Session, dueTo error: Error, completion: @escaping (RetryResult) -> Void) {
+  func retry(
+    _ request: Request,
+    for session: Session,
+    dueTo error: Error,
+    completion: @escaping (RetryResult) -> Void
+  ) {
+
     guard
       let response = request.task?.response as? HTTPURLResponse,
       request.retryCount < 3,
       response.statusCode == 401
     else {
+      completion(.doNotRetry)
       return
+    }
+
+    let token = Token(
+      accessToken: Keychain.shared.getAccessToken() ?? "",
+      refreshToken: Keychain.shared.getRefreshToken() ?? ""
+    )
+
+    NetworkService.shared.authRepository.refresh(token) { res, err in
+
+      guard let res = res else {
+        completion(.retry)
+        return
+      }
+
+      if res.status == 200 {
+        Keychain.shared.setAccessToken(accessToken: res.data?.token.accessToken)
+        Keychain.shared.setRefreshToken(refreshToken: res.data?.token.refreshToken)
+        completion(.retry)
+      }
     }
   }
 }
