94fa7858eb3217d1dcaba845c73fe081e205401d
psychehose <psychehose@gmail.com>
2023-02-14 18:35:48 +0900

    [#153] Chore: white trailing 제거

--- Hous-iOS-release/Scene/OurRule/EditRuleView/EditRuleViewController.swift
+++ Hous-iOS-release/Scene/OurRule/EditRuleView/EditRuleViewController.swift
@@ -13,43 +13,43 @@ import RxKeyboard
 import Network
 import BottomSheetKit
 
-class EditRuleViewController: LoadingBaseViewController {
-  
+final class EditRuleViewController: LoadingBaseViewController {
+
   //MARK: - UI Components
-  
+
   private let navigationBar: NavBarWithBackButtonView = {
     let navBar = NavBarWithBackButtonView(title: "Rules 수정")
     navBar.setRightButtonText(text: "저장")
     return navBar
   }()
-  
+
   private let rulesTableView = UITableView().then {
     $0.showsVerticalScrollIndicator = false
     $0.allowsSelectionDuringEditing = true
     $0.isEditing = true
   }
-  
+
   private let ruleEmptyViewLabel = UILabel().then {
     $0.isHidden = true
     $0.text = "아직 우리 집 Rules가 없어요!"
     $0.font = Fonts.SpoqaHanSansNeo.medium.font(size: 14)
     $0.textColor = Colors.g5.color
   }
-  
+
   //MARK: - Var & lets
-  
+
   private let disposeBag = DisposeBag()
-  
+
   private let saveButtonDidTapped = PublishSubject<[RuleWithIdViewModel]>()
-  
+
   private let viewDidTapped = PublishSubject<Void>()
-  
+
   private var editViewRules: [RuleWithIdViewModel]
-  
+
   private let viewModel: EditRuleViewModel
-  
+
   private var tabBarHeight: CGFloat = 83
-  
+
   init(editViewRules: [RuleWithIdViewModel], viewModel: EditRuleViewModel) {
     self.editViewRules = editViewRules
     self.viewModel = viewModel
@@ -69,7 +69,11 @@ class EditRuleViewController: LoadingBaseViewController {
   }
   
   private func setTableView() {
-    rulesTableView.register(EditRuleTableViewCell.self, forCellReuseIdentifier: EditRuleTableViewCell.className)
+    rulesTableView.register(
+      EditRuleTableViewCell.self,
+      forCellReuseIdentifier: EditRuleTableViewCell.className
+    )
+
     rulesTableView.rx.setDelegate(self).disposed(by: disposeBag)
   }
   
@@ -85,59 +89,59 @@ class EditRuleViewController: LoadingBaseViewController {
   
   private func configUI() {
     navigationBar.updateRightButtonSnapKit()
-    
+
     self.view.addSubViews([
       navigationBar,
       rulesTableView,
       ruleEmptyViewLabel
     ])
-    
+
     configLoadingLayout()
-    
+
     navigationBar.snp.makeConstraints { make in
       make.top.equalTo(view.safeAreaLayoutGuide)
       make.leading.trailing.equalToSuperview()
       make.height.equalTo(60)
     }
-    
+
     rulesTableView.snp.makeConstraints { make in
       make.top.equalTo(navigationBar.snp.bottom)
       make.leading.trailing.equalToSuperview()
       make.bottom.equalTo(self.view.safeAreaLayoutGuide)
     }
-    
+
     ruleEmptyViewLabel.snp.makeConstraints { make in
       make.top.equalTo(navigationBar.snp.bottom).offset(48)
       make.centerX.equalToSuperview()
     }
-    
+
     setEmptyView()
   }
-  
+
   //MARK: - Bind
   private func bind() {
     let observable = Observable.just(self.editViewRules)
-    
+
     observable
       .bind(to: rulesTableView.rx.items(cellIdentifier: EditRuleTableViewCell.className, cellType: EditRuleTableViewCell.self)) { [weak self] row, _, cell in
         guard let self = self else { return }
-        
+
         let viewModel = self.editViewRules[row]
-        
+
         cell.setTextFieldData(rule: viewModel.name)
-        
+
         if row < 3 {
           cell.backgroundColor = Colors.blueL2.color
         } else {
           cell.backgroundColor = Colors.white.color
         }
-        
+
         let existing = cell.backgroundColor
-        
+
         cell.todoLabelTextField.rx.controlEvent([.editingDidBegin, .editingDidEnd])
           .asDriver()
           .drive(onNext: { _ in
-            
+
             let editBlueColor = Colors.blueEdit.color
             
             if cell.backgroundColor == existing {
@@ -146,39 +150,39 @@ class EditRuleViewController: LoadingBaseViewController {
               } else {
                 cell.backgroundColor = Colors.g3.color
               }
-              
+
             } else {
               cell.backgroundColor = existing
             }
           })
           .disposed(by: cell.disposeBag)
-        
+
         self.viewDidTapped
           .asDriver(onErrorJustReturn: ())
           .drive(onNext: { _ in
             cell.todoLabelTextField.endEditing(true)
           })
           .disposed(by: cell.disposeBag)
-        
+
         cell.todoLabelTextField.rx.text
           .distinctUntilChanged()
           .asObservable()
           .subscribe(onNext: { [weak self] str in
             guard let self = self else { return }
             let changedName = str.map { $0 }
-            
+
             let ruleWithIdViewModel = RuleWithIdViewModel(id: viewModel.id, name: changedName ?? "")
-            
+
             self.editViewRules[row] = ruleWithIdViewModel
-            
+
           })
           .disposed(by: cell.disposeBag)
-        
+
         cell.separatorInset = UIEdgeInsets(top: 0, left: 24, bottom: 0, right: 24)
         cell.selectionStyle = .none
       }
       .disposed(by: disposeBag)
-    
+
     rulesTableView.rx.itemMoved
       .map { $0 }
       .subscribe (onNext: { [weak self] event in
@@ -187,7 +191,7 @@ class EditRuleViewController: LoadingBaseViewController {
         self.rulesTableView.reloadData()
       })
       .disposed(by: disposeBag)
-    
+
     view.rx
       .tapGesture()
       .asObservable()
@@ -196,12 +200,12 @@ class EditRuleViewController: LoadingBaseViewController {
         self.viewDidTapped.onNext(())
       })
       .disposed(by: disposeBag)
-    
+
     RxKeyboard.instance.visibleHeight
       .skip(1)
       .drive(onNext: { [weak self] height in
         guard let self = self else { return }
-        
+
         let firstWindow = UIApplication.shared.connectedScenes
           .filter { $0.activationState == .foregroundActive }
           .map { $0 as? UIWindowScene }
@@ -209,14 +213,14 @@ class EditRuleViewController: LoadingBaseViewController {
           .first?.windows
           .filter { $0.isKeyWindow }
           .first
-        
+
         let extra = firstWindow!.safeAreaInsets.bottom
         self.rulesTableView.snp.updateConstraints { make in
           make.bottom.equalTo(self.view.safeAreaLayoutGuide).inset(height - extra)
         }
       })
       .disposed(by: disposeBag)
-    
+
     let input = EditRuleViewModel.Input(
       backButtonDidTap: navigationBar.backButton.rx.tap
         .asObservable(),
@@ -225,9 +229,9 @@ class EditRuleViewController: LoadingBaseViewController {
         self?.showLoading()
       })
     )
-    
+
     let output = viewModel.transform(input: input)
-    
+
     output.saveCompleted
       .do(onNext: { [weak self] _ in
         self?.hideLoading()
@@ -236,7 +240,7 @@ class EditRuleViewController: LoadingBaseViewController {
         self?.navigationController?.popViewController(animated: true)
       })
       .disposed(by: disposeBag)
-    
+
     output.moveToRuleMainView
       .drive(onNext: { [weak self] _ in
         guard let self = self else { return }
@@ -258,13 +262,9 @@ class EditRuleViewController: LoadingBaseViewController {
         }
       })
       .disposed(by: disposeBag)
-    
-  }
-
-  private func upTableView() {
 
   }
-  
+
   private func configButtonAction() {
     navigationBar.rightButton.rx.tap
       .subscribe(onNext: { [weak self] _ in
@@ -282,11 +282,17 @@ extension EditRuleViewController: UITableViewDelegate {
     return 52
   }
 
-  func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+  func tableView(
+    _ tableView: UITableView,
+    editingStyleForRowAt indexPath: IndexPath
+  ) -> UITableViewCell.EditingStyle {
     return .none
   }
 
-  func tableView(_ tableView: UITableView, shouldIndentWhileEditingRowAt indexPath: IndexPath) -> Bool {
-      return false
+  func tableView(
+    _ tableView: UITableView,
+    shouldIndentWhileEditingRowAt indexPath: IndexPath
+  ) -> Bool {
+    return false
   }
 }
