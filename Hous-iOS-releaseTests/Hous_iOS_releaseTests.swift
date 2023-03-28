b51659680c630f8d272de53735b2689c1593bb31
psychehose <psychehose@gmail.com>
2023-03-28 17:24:22 +0900

    [#161] ADD: Hello Test Case

--- Hous-iOS-releaseTests/Hous_iOS_releaseTests.swift
+++ Hous-iOS-releaseTests/Hous_iOS_releaseTests.swift
@@ -6,15 +6,31 @@
 //
 
 import XCTest
+@testable import Hous_iOS_release
 
 final class Hous_iOS_releaseTests: XCTestCase {
 
+  var sut: UpdateTodoViewController!
+
   override func setUpWithError() throws {
+    try super.setUpWithError()
+
+    let ho = UpdateTodoHomieModel(
+      name: "김호세", color: .BLUE, selectedDay: [], onboardingID: 25, isExpanded: true)
+
+    let reactor = UpdateTodoReactor(provider: ServiceProvider(), state: .init(todoHomies: [ho]))
+    sut = UpdateTodoViewController(reactor)
   }
 
   override func tearDownWithError() throws {
+    try super.tearDownWithError()
   }
 
+
   func testExample() throws {
+
+    let view = sut.view.asImage()
+
   }
+
 }
