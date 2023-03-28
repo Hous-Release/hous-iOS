4754b3ed2037b595360a2652e9a105f77d26b623
psychehose <psychehose@gmail.com>
2023-03-28 14:31:12 +0900

    [#161] ADD: 유닛 테스트 타겟 추가 및 테스트 타겟 lint 처리

---
+++ Hous-iOS-releaseTests/Hous_iOS_releaseTests.swift
@@ -0,0 +1,20 @@
+//
+//  Hous_iOS_releaseTests.swift
+//  Hous-iOS-releaseTests
+//
+//  Created by 김호세 on 2023/03/28.
+//
+
+import XCTest
+
+final class Hous_iOS_releaseTests: XCTestCase {
+
+  override func setUpWithError() throws {
+  }
+
+  override func tearDownWithError() throws {
+  }
+
+  func testExample() throws {
+  }
+}
