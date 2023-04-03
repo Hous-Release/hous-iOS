13beb7d64fd3111b6aa3e72c05174f841ae4a1f7
psychehose <psychehose@gmail.com>
2023-04-03 17:06:38 +0900

    TEST ADD: UpdateTodoReactor fetch 테스트 코드 추가

--- Hous-iOS-releaseTests/Hous_iOS_releaseTests.swift
+++ Hous-iOS-releaseTests/Hous_iOS_releaseTests.swift
@@ -5,32 +5,57 @@
 //  Created by 김호세 on 2023/03/28.
 //
 
+import Foundation
+import ReactorKit
+import RxTest
+import RxBlocking
 import XCTest
+
 @testable import Hous_iOS_release
+import UserInformation
 
 final class Hous_iOS_releaseTests: XCTestCase {
 
-  var sut: UpdateTodoViewController!
+  var todoHomies: [UpdateTodoHomieModel]!
+  var service: ServiceProvider!
 
   override func setUpWithError() throws {
     try super.setUpWithError()
 
-    let ho = UpdateTodoHomieModel(
-      name: "김호세", color: .BLUE, selectedDay: [], onboardingID: 25, isExpanded: true)
+    service = ServiceProvider()
+    todoHomies = []
 
-    let reactor = UpdateTodoReactor(provider: ServiceProvider(), state: .init(todoHomies: [ho]))
-    sut = UpdateTodoViewController(reactor)
+    for i in 0 ..< 7 {
+      let updateTodoHomieModel = UpdateTodoHomieModel(
+        name: "김호세\(i)",
+        color: .BLUE,
+        selectedDay: [],
+        onboardingID: i,
+        isExpanded: false
+      )
+
+      todoHomies.append(updateTodoHomieModel)
+
+    }
   }
 
+
   override func tearDownWithError() throws {
+    todoHomies = nil
+    service = nil
     try super.tearDownWithError()
   }
 
 
-  func testExample() throws {
+  func testAction_fetch() {
 
-    let view = sut.view.asImage()
+    let reactor = UpdateTodoReactor(provider: service, state: .init(todoHomies: todoHomies))
+    reactor.action.onNext(.fetch)
 
+    XCTAssertEqual(reactor.currentState.todoHomies.count, 7)
+    XCTAssertEqual(reactor.currentState.didTappedIndividual, nil)
+    XCTAssertEqual(reactor.currentState.isModifying, false)
+    XCTAssertEqual(reactor.currentState.isPushNotification, true)
+    XCTAssertEqual(reactor.currentState.todoHomies[0].name, "김호세0")
   }
-
 }
