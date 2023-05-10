c76e36f751e7bbfb17e3d652d9ad775e2ede92a4
Kim Minjae <ffalswo2@naver.com>
2023-05-10 21:58:13 +0900

    [#181] ADD: BaseView 추가

--- Hous-iOS-release/Base/BaseView.swift
+++ Hous-iOS-release/Base/BaseView.swift
@@ -9,12 +9,18 @@ import UIKit
 
 class BaseView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    setStyle()
+    setLayout()
+  }
 
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  func setStyle() {}
+
+  /// Constraints (계층 및 제약조건) 설정 메서드
+  func setLayout() {}
 }
