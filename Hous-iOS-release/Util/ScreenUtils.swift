f16e7f87f79bc771209ec229118b3acc38aef8d2
Kim Minjae <ffalswo2@naver.com>
2023-05-14 15:31:29 +0900

    [#185] ADD: ScreenUtils 추가

--- Hous-iOS-release/Util/ScreenUtils.swift
+++ Hous-iOS-release/Util/ScreenUtils.swift
@@ -5,4 +5,18 @@
 //  Created by 김민재 on 2023/05/14.
 //
 
-import Foundation
+import UIKit
+
+final class ScreenUtils {
+    static func getWidth(_ value: CGFloat) -> CGFloat {
+        let width = UIScreen.main.bounds.width
+        let standardWidth: CGFloat = 375.0
+        return width / standardWidth * value
+    }
+
+    static func getHeight(_ value: CGFloat) -> CGFloat {
+        let height = UIScreen.main.bounds.height
+        let standardheight: CGFloat = 812.0
+        return height / standardheight * value
+    }
+}
