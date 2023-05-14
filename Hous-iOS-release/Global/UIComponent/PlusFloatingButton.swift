d61a89b0ad4b96d5fec6a205227b8a0673e9dad0
Kim Minjae <ffalswo2@naver.com>
2023-05-14 15:30:08 +0900

    [#185] ADD: 파일 추가
    - PlusFloatingButton.swift
    - ScreenUtils.swift

---
+++ Hous-iOS-release/Global/UIComponent/PlusFloatingButton.swift
@@ -0,0 +1,20 @@
+//
+//  PlusFloatingButton.swift
+//  Hous-iOS-release
+//
+//  Created by 김민재 on 2023/05/14.
+//
+
+import UIKit
+
+class PlusFloatingButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Hous-iOS-release/Util/ScreenUtils.swift
@@ -0,0 +1,8 @@
+//
+//  ScreenUtils.swift
+//  Hous-iOS-release
+//
+//  Created by 김민재 on 2023/05/14.
+//
+
+import Foundation
