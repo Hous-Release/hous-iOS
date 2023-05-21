61c170264707cb982845788d8951a8ba6c3fbb02
Jihyun247 <wlguslove12345@gmail.com>
2023-05-21 21:07:59 +0900

    [#189] FEAT: HousButton - OnboardingButton

--- HousUIComponent/Sources/HousButton/OnboardingButton.swift
+++ HousUIComponent/Sources/HousButton/OnboardingButton.swift
@@ -8,29 +8,57 @@
 import UIKit
 import AssetKit
 
-internal final class OnboardingButton: UIButton {
+public final class OnboardingButton: UIButton {
 
-    private typealias SpoqaHanSansNeo = Fonts.SpoqaHanSansNeo
+  init(_ type: Button.Onboarding) {
+    super.init(frame: .zero)
+    configUI(type)
+  }
 
-    override var isEnabled: Bool {
-        didSet {
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
 
+  private func configUI(_ type: Button.Onboarding) {
+    configuration = .bordered()
+
+    var attrString = AttributedString(type.rawValue)
+    attrString.font = Fonts.SpoqaHanSansNeo.bold.font(size: 18)
+    attrString.foregroundColor = Colors.white.color
+    configuration?.attributedTitle = attrString
+
+    configuration?.background.cornerRadius = 0
+    configuration?.cornerStyle = .fixed
+    configuration?.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 20, trailing: 0)
+
+    updateHandlerByType(type)
+  }
+
+  private func updateHandlerByType(_ type: Button.Onboarding) {
+
+    configurationUpdateHandler = { btn in
+      switch btn.state {
+      case .normal:
+        switch type {
+        case .next, .createRoom:
+          btn.configuration?.baseBackgroundColor = Colors.blue.color
+        case .leaveProfile:
+          btn.configuration?.baseBackgroundColor = Colors.red.color
         }
+
+      case .disabled:
+        switch type {
+        case .next, .createRoom:
+          btn.configuration?.baseBackgroundColor = Colors.g4.color
+        case .leaveProfile:
+          btn.configuration?.baseBackgroundColor = Colors.redB1.color
+        }
+
+      default:
+        break
+      }
     }
 
-    
+  }
 
-    internal override init(frame: CGRect) {
-        super.init(frame: .zero)
-    }
-
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
-    private func configUI() {
-
-        self.titleLabel?.font = SpoqaHanSansNeo.bold.font(size: 18)
-        self.setTitleColor(.white, for: [.normal, .disabled])
-    }
 }
