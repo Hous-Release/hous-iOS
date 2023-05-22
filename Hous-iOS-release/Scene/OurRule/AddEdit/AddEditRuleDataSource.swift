e1cfb6de436493d81f264787b1f8235e86c3e1b7
Kim Minjae <ffalswo2@naver.com>
2023-05-22 11:29:03 +0900

    [#188] ADD - AddEditRule 파일 및 에셋 추가

---
+++ Hous-iOS-release/Scene/OurRule/AddEdit/AddEditRuleDataSource.swift
@@ -0,0 +1,8 @@
+//
+//  AddEditRuleDataSource.swift
+//  Hous-iOS-release
+//
+//  Created by 김민재 on 2023/05/21.
+//
+
+import Foundation
---
+++ Hous-iOS-release/Scene/OurRule/AddEdit/Cells/TitleDetailCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  TitleDetailCollectionViewCell.swift
+//  Hous-iOS-release
+//
+//  Created by 김민재 on 2023/05/21.
+//
+
+import UIKit
+
+class TitleDetailCollectionViewCell: UICollectionViewCell {
+    
+}
