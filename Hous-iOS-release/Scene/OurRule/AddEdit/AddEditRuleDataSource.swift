4971e2fbd7a8207fa774e096480c8300869138a6
Kim Minjae <ffalswo2@naver.com>
2023-05-22 11:29:51 +0900

    [#188] WIP - AddEditRule UI 구현 중

--- Hous-iOS-release/Scene/OurRule/AddEdit/AddEditRuleDataSource.swift
+++ Hous-iOS-release/Scene/OurRule/AddEdit/AddEditRuleDataSource.swift
@@ -5,4 +5,18 @@
 //  Created by 김민재 on 2023/05/21.
 //
 
-import Foundation
+import UIKit
+
+struct RulePhoto: Hashable {
+  let image: UIImage
+  let identifier = UUID()
+
+  func hash(into hasher: inout Hasher) {
+    hasher.combine(identifier)
+  }
+
+  static func == (lhs: RulePhoto, rhs: RulePhoto) -> Bool {
+    return lhs.identifier == rhs.identifier
+  }
+
+}
