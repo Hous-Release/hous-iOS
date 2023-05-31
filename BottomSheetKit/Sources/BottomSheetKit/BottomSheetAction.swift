538c25394ef39fd31c656d3d157daf4ce13a0203
Kim Minjae <ffalswo2@naver.com>
2023-06-01 03:59:49 +0900

    [#195] ADD: BottomSheetAction protocol 변경
    - associatedtype, view 추가

--- BottomSheetKit/Sources/BottomSheetKit/BottomSheetAction.swift
+++ BottomSheetKit/Sources/BottomSheetKit/BottomSheetAction.swift
@@ -5,12 +5,15 @@
 //  Created by 김호세 on 2022/11/10.
 //
 
-import Foundation
+import UIKit
 
 public typealias CompleteBottomSheetAction = ((DidBottomSheetActionType) -> Void)
 
 protocol BottomSheetAction {
-  func sendAction(_ action: DidBottomSheetActionType)
-  func cancel()
-  var completeAction: CompleteBottomSheetAction? { get }
+    associatedtype T: UIView
+    func sendAction(_ action: DidBottomSheetActionType)
+    func cancel()
+    var completeAction: CompleteBottomSheetAction? { get }
+    var view: T { get set }
+    
 }
