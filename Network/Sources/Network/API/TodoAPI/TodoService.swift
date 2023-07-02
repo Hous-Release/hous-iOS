e6de8691c609eef1731b3ce454c345df0a32ad91
Jihyun247 <wlguslove12345@gmail.com>
2023-07-02 17:49:56 +0900

    [#191] FEAT: filter todo 서버통신 DTO 수정 및 fetchDataToPublish 메서드 생성 및 api & service 적용

--- Network/Sources/Network/API/TodoAPI/TodoService.swift
+++ Network/Sources/Network/API/TodoAPI/TodoService.swift
@@ -1,8 +1,53 @@
 //
-//  File.swift
+//  TodoService.swift
 //  
 //
 //  Created by 김지현 on 2023/06/30.
 //
 
 import Foundation
+import Alamofire
+
+public enum NewTodoService {
+
+  // 우선 combine 적용하는 todo FilterView 관련 서버통신만 마이그레이션
+  case getTodoByFilter(
+    _ dto: FilteredTodoDTO.Request.getTodosFilteredRequestDTO
+  )
+  case getTodoDetail(
+    _ todoId: Int
+  )
+}
+
+extension NewTodoService: TargetType {
+  public var baseURL: String {
+    return API.apiBaseURL
+  }
+
+  public var path: String {
+    switch self {
+    case .getTodoByFilter:
+      return "/todos"
+    case .getTodoDetail(let todoId):
+        return "/todo/\(todoId)/summary"
+    }
+  }
+
+  public var method: HTTPMethod {
+    switch self {
+    case .getTodoByFilter:
+      return .get
+    case .getTodoDetail:
+        return .get
+    }
+  }
+
+  public var parameters: RequestParams {
+    switch self {
+    case .getTodoByFilter(let dto) :
+      return .body(dto)
+    case .getTodoDetail:
+      return .requestPlain
+    }
+  }
+}
