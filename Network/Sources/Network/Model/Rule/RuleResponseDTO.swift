98fcd38da0b1a62b3b2c82085b68c7f2f7c1093b
Kim Minjae <ffalswo2@naver.com>
2023-08-28 04:12:36 +0900

    [#213] FEAT: 규칙 조회 API 응답 모델 생성

--- Network/Sources/Network/Model/Rule/RuleResponseDTO.swift
+++ Network/Sources/Network/Model/Rule/RuleResponseDTO.swift
@@ -9,6 +9,14 @@ public extension RuleDTO.Response {
   struct RuleResponseDTO: Decodable {
     public let rules: [Rule]
   }
+
+  struct SingleRuleResponseDTO: Decodable {
+    public let id: Int
+    public let name: String
+    public let description: String
+    public let images: [String]
+    public let updatedAt: String
+  }
   
   struct Rule: Decodable {
     public let id: Int
