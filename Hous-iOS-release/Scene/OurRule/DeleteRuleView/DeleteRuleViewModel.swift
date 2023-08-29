cf6cc1657ac8bf17e69d34143dfdeff14580780c
Kim Minjae <ffalswo2@naver.com>
2023-08-29 18:26:13 +0900

    [#213] CHORE: 추후 삭제해야할 파일 주석처리

--- Hous-iOS-release/Scene/OurRule/DeleteRuleView/DeleteRuleViewModel.swift
+++ Hous-iOS-release/Scene/OurRule/DeleteRuleView/DeleteRuleViewModel.swift
@@ -10,30 +10,30 @@ import RxSwift
 import RxCocoa
 import Network
 
-class DeleteRuleViewModel: ViewModelType {
-  struct Input {
-    let deleteButtonDidTapped: Observable<[Int]>
-    let navBackButtonDidTapped: Observable<Void>
-  }
+// class DeleteRuleViewModel: ViewModelType {
+//  struct Input {
+//    let deleteButtonDidTapped: Observable<[Int]>
+//    let navBackButtonDidTapped: Observable<Void>
+//  }
+//
+//  struct Output {
+//    let deletedCompleted: Driver<Void>
+//    let moveToMainRuleView: Driver<Void>
+//  }
 
-  struct Output {
-    let deletedCompleted: Driver<Void>
-    let moveToMainRuleView: Driver<Void>
-  }
+//  func transform(input: Input) -> Output {
+//    let deletedCompleted = input.deleteButtonDidTapped.flatMap { ruleId in
+//      return NetworkService.shared
+//        .ruleRepository
+//        .deleteRules(
+//        RuleDTO.Request.deleteRulesRequestDTO(rulesIdList: ruleId))
+//    }
+//    .asDriver(onErrorJustReturn: ())
+//
+//    return Output(
+//      deletedCompleted: deletedCompleted,
+//      moveToMainRuleView: input.navBackButtonDidTapped.asDriver(onErrorJustReturn: ())
+//    )
 
-  func transform(input: Input) -> Output {
-    let deletedCompleted = input.deleteButtonDidTapped.flatMap { ruleId in
-      return NetworkService.shared
-        .ruleRepository
-        .deleteRules(
-        RuleDTO.Request.deleteRulesRequestDTO(rulesIdList: ruleId))
-    }
-    .asDriver(onErrorJustReturn: ())
-
-    return Output(
-      deletedCompleted: deletedCompleted,
-      moveToMainRuleView: input.navBackButtonDidTapped.asDriver(onErrorJustReturn: ())
-    )
-
-  }
-}
+//  }
+// }
