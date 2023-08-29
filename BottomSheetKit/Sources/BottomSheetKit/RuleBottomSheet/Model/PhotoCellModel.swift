bb06844c6d59871b682b2b7f2d39f9996483e06a
Kim Minjae <ffalswo2@naver.com>
2023-08-30 00:34:52 +0900

    [#213] CHORE: BottomSheetKit RulePhoto 수정

--- BottomSheetKit/Sources/BottomSheetKit/RuleBottomSheet/Model/PhotoCellModel.swift
+++ BottomSheetKit/Sources/BottomSheetKit/RuleBottomSheet/Model/PhotoCellModel.swift
@@ -9,19 +9,22 @@ import Foundation
 
 
 public struct PhotoCellModel {
+    public let ruleId: Int?
     public let title: String
     public let description: String?
     public let lastmodifedDate: String
     public let photos: [RulePhoto]?
 
     public init(
+        ruleId: Int? = nil,
         title: String,
         description: String?,
         lastmodifedDate: String,
         photos: [RulePhoto]?) {
-        self.title = title
-        self.description = description
-        self.lastmodifedDate = lastmodifedDate
-        self.photos = photos
-    }
+            self.ruleId = ruleId
+            self.title = title
+            self.description = description
+            self.lastmodifedDate = lastmodifedDate
+            self.photos = photos
+        }
 }
