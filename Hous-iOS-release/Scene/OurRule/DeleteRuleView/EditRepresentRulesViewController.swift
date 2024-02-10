6dcdd70a3e1c761dd2173000a15659e5df790a86
MINJAE KIM <ffalswo2@naver.com>
2024-02-10 22:18:00 +0900

    [FEAT] 대표 규칙 설정 기능 구현 (#214)
    - 수정 후 이탈 시 팝업
    - 네트워크 연결

--- Hous-iOS-release/Scene/OurRule/DeleteRuleView/EditRepresentRulesViewController.swift
+++ Hous-iOS-release/Scene/OurRule/DeleteRuleView/EditRepresentRulesViewController.swift
@@ -1,5 +1,5 @@
 //
-//  DeleteRuleViewController.swift
+//  EditRepresentRulesViewController.swift
 //  Hous-iOS-release
 //
 //  Created by 김민재 on 2022/11/04.
--- Hous-iOS-release/Scene/OurRule/DeleteRuleView/EditRepresentRulesViewModel.swift
+++ Hous-iOS-release/Scene/OurRule/DeleteRuleView/EditRepresentRulesViewModel.swift
@@ -1,5 +1,5 @@
 //
-//  DeleteRuleViewModel.swift
+//  EditRepresentRulesViewModel.swift
 //  Hous-iOS-release
 //
 //  Created by 김민재 on 2022/11/04.
@@ -14,26 +14,68 @@ import Network
   struct Input {
     let saveButtonDidTap: Observable<[Int]>
     let navBackButtonDidTapped: Observable<Void>
+    let ruleDidTap: Observable<Int>
   }
 
   struct Output {
     let savedComplete: Driver<Void>
-    let moveToMainRuleView: Driver<Void>
+    let moveToMainRuleView: Driver<Bool>
+    let representCountExceeded: PublishSubject<Bool>
   }
 
+   private let maxRepresentCount = 3
+
+   private let rules: [HousRule] = []
+
+   private var selectedRulesId: [Int] = []
+
+   private var disposeBag = DisposeBag()
+
   func transform(input: Input) -> Output {
-    let savedComplete = input.saveButtonDidTap.flatMap { ruleId in
-//      return NetworkService.shared
-//        .ruleRepository
-//        .deleteRules(
-//        RuleDTO.Request.deleteRulesRequestDTO(rulesIdList: ruleId)
-//        )
+    let representCountExceeded = PublishSubject<Bool>()
+    let updateRequest = PublishSubject<[Int]>()
+
+    input.ruleDidTap
+      .subscribe(onNext: { [weak self] ruleId in
+        guard let self else { return }
+        if self.selectedRulesId.contains(ruleId) {
+          self.selectedRulesId.removeFirst(ruleId)
+          return
+        }
+        self.selectedRulesId.append(ruleId)
+      })
+      .disposed(by: disposeBag)
+
+    input.saveButtonDidTap
+      .subscribe(onNext: { ruleIds in
+        if ruleIds.count > self.maxRepresentCount {
+          representCountExceeded.onNext(true)
+          return
+        }
+        updateRequest.onNext(ruleIds)
+      })
+      .disposed(by: disposeBag)
+
+    let naviBackButtonDidTap = input.navBackButtonDidTapped
+      .map { _ in
+        return !self.selectedRulesId.isEmpty
+      }
+      .asDriver(onErrorJustReturn: false)
+
+    let savedComplete = updateRequest
+      .flatMap { ruleIds in
+        self.selectedRulesId = ruleIds
+      let requestDTO = RuleDTO.Request.RepresentRulesRequestDTO(rules: ruleIds)
+      return NetworkService.shared
+        .ruleRepository
+        .updateRepresentRules(requestDTO)
     }
-    .asDriver(onErrorJustReturn: ())
+      .asDriver(onErrorJustReturn: ())
 
     return Output(
       savedComplete: savedComplete,
-      moveToMainRuleView: input.navBackButtonDidTapped.asDriver(onErrorJustReturn: ())
+      moveToMainRuleView: naviBackButtonDidTap,
+      representCountExceeded: representCountExceeded
     )
 
   }
